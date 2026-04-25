0a034f1f1bd2c9050ad4204ef24077e1c9ad3748
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-04-25 16:47:00 -0700

    CREATE project factory

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => fake()->unique()->numberBetween(),
+            'name' => fake()->name(),
+            'url' => fake()->unique()->url(),
+            'description' => fake()->paragraphs(1),
+        ];
+    }
+}
