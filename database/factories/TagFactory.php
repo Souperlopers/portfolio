cd29263ff72d59225e13ff52b406517c5bc0602d
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-04-25 16:54:55 -0700

    CREATE tag factory

---
+++ database/factories/TagFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Member;
+use App\Models\Project;
+use App\Models\Tag;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tag>
+ */
+class TagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $taggable = fake()->randomElement([
+            Member::class,
+            Project::class,
+        ]);
+
+        $related = $taggable::inRandomOrder()->first();
+
+        return [
+            'name' => fake()->word(),
+            'type' => fake()->randomElement(Tag::VALUES),
+            'version' => fake()->optional()->word(),
+            'taggable_id' => $related->id,
+            'taggable_type' => $taggable,
+        ];
+    }
+}
