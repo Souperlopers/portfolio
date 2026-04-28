e56d72a486b8922772773c3a11ed4f79522423be
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-04-27 17:12:44 -0700

    CHANGE back to TailwindCSS JIT

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Souperlopers')
 
 @section('body')
-	<h1 class="text-3xl font-bold underline">Banner</h1>
+	<h1 class="text-3xl font-bold underline bg-red-300">Banner</h1>
 	<h1><a href="/projects">Projects</a></h1>
 	<h1><a href="/members">Members</a></h1>
 	<h1>Reviews</h1>
