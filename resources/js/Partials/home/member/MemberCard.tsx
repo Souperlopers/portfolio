ab1a57f7498e40e7eaddf2f16d99fd35a40e4993
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-29 18:48:12 +0330

    STYLE truncate texts

--- resources/js/Partials/home/member/MemberCard.tsx
+++ resources/js/Partials/home/member/MemberCard.tsx
@@ -3,6 +3,9 @@ import { User } from "@/types";
 
 const MemberCard = ({ data }: { data: User }) => {
     const userId = data.id;
+    const name = data.name || "بدون نام";
+    const position = data.position || "بدون سمت";
+
     return (
         <div className="bg-white p-5 rounded w-full">
             <div className="flex lg:flex-row flex-col gap-5 w-full">
@@ -10,10 +13,10 @@ const MemberCard = ({ data }: { data: User }) => {
                     <div className="bg-pink-300 w-32 h-32 rounded-full"></div>
                 </div>
                 <div className="flex lg:justify-start justify-center">
-                    <div className="flex flex-col pt-5 gap-3 text-center">
-                        <span>{data.name || "-"}</span>
-                        <span>{data.position || "-"}</span>
-                        <Link href={route("member", { userSlug: userId })}>
+                    <div className="flex flex-col pt-5 gap-3 lg:text-right text-center">
+                        <span className="truncate lg:max-w-32 max-w-10">{name}</span>
+                        <span className="truncate lg:max-w-32 max-w-10">{position}</span>
+                        <Link href={route("member", { userSlug: userId })} className="text-cyan-500 font-semibold">
                             مشاهده پروفایل
                         </Link>
                     </div>
