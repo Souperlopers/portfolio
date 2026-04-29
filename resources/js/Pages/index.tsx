f483a4edef0441ed5d1262b0452916ae636afd1a
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-29 17:38:10 +0330

    UPDATE folder structure

--- resources/js/Pages/index.tsx
+++ resources/js/Pages/index.tsx
@@ -1,3 +1,3 @@
-export {default as Banner} from "./Home/components/Banner";
-export {default as Projects} from "./Home/components/projects/Projects";
-export {default as Members} from "./Home/components/member/Members";
+export {default as Banner} from "@/Partials/home/Banner";
+export {default as Projects} from "@/Partials/home/projects/Projects";
+export {default as Members} from "@/Partials/home/member/Members";
