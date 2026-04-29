fc4d5038761352af993cc3b9f107d1e0168df206
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-29 14:34:18 +0330

    ADD pass real data to Projects component

--- resources/js/Pages/Home/Home.tsx
+++ resources/js/Pages/Home/Home.tsx
@@ -2,24 +2,22 @@ import { ReactNode } from "react";
 import { Link, Head } from "@inertiajs/react";
 import MainLayout from "@/Layouts/MainLayout";
 import { Banner, Projects } from "../index";
+import { Project } from "@/types";
 
 export default function Home({
     projects,
     members,
 }: {
-    projects: object;
+    projects: Project[];
     members: object;
 }) {
+    console.log(projects);
     return (
         <div>
             <Head title="SouperLopers" />
             <div className="flex flex-col gap-5">
-                <div className="h-52">
-                    <Banner />
-                </div>
-                <div className="h-64">
-                    <Projects />
-                </div>
+                <Banner />
+                <Projects projects={projects} />
                 <div className="rounded bg-orange-400 h-48">Members</div>
             </div>
         </div>
