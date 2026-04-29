a1b01dac364072a572b31c9bd39a45c955f96bf7
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-29 14:33:48 +0330

    STYLE  change padding

--- resources/js/Pages/Home/components/Banner.tsx
+++ resources/js/Pages/Home/components/Banner.tsx
@@ -1,15 +1,15 @@
 import banner from "../../../../../public/assets/banner.jpg";
 const Banner = () => {
     return (
-        <div className="h-full relative rounded bg-black">
-            <div className="flex w-full h-full">
+        <div className="h-full w-full relative rounded bg-[#1a222c]">
+            <div className="flex justify-end w-full h-full">
                 <img src={banner} alt="banner" className="object-center rounded" />
             </div>
-            <div className="absolute bottom-5 -left-0.5 mx-11 py-2 w-[35.9%] flex justify-between text-white font-bold bg-[#1a222c]">
-                <button className="bg-gray-200 px-10 py-3 rounded font-semibold text-lg text-[#1a222c] cursor-pointer">
+            <div className="absolute bottom-0 -left-1 mx-11 py-2 w-[32.9%] flex justify-between text-white font-bold bg-[#1a222c]">
+                <button className="bg-gray-200 px-5 py-3 rounded font-semibold text-lg text-[#1a222c] cursor-pointer">
                     دریافت رزومه
                 </button>
-                <button className="bg-cyan-600 px-16 py-3 rounded font-semibold text-lg text-white cursor-pointer">
+                <button className="bg-cyan-600 px-10 py-3 rounded font-semibold text-lg text-white cursor-pointer">
                     تماس با ما
                 </button>
             </div>
