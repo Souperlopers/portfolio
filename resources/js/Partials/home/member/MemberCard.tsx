c533851e5e33fdd2997de325e146fb568dfe0853
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-29 18:42:16 +0330

    STYLE MemberCard responsive

--- resources/js/Partials/home/member/MemberCard.tsx
+++ resources/js/Partials/home/member/MemberCard.tsx
@@ -4,15 +4,19 @@ import { User } from "@/types";
 const MemberCard = ({ data }: { data: User }) => {
     const userId = data.id;
     return (
-        <div className="bg-white p-5 rounded flex justify-center w-full">
-            <div className="flex  gap-5 w-full">
-                <div className="bg-pink-300 w-32 h-32 rounded-full"></div>
-                <div className="flex flex-col pt-5 gap-3">
-                    <span>{data.name || "-"}</span>
-                    <span>{data.position || "-"}</span>
-                    <Link href={route('member',{userSlug:userId})}>
-                    مشاهده پروفایل
-                    </Link>
+        <div className="bg-white p-5 rounded w-full">
+            <div className="flex lg:flex-row flex-col gap-5 w-full">
+                <div className="flex lg:justify-start justify-center">
+                    <div className="bg-pink-300 w-32 h-32 rounded-full"></div>
+                </div>
+                <div className="flex lg:justify-start justify-center">
+                    <div className="flex flex-col pt-5 gap-3 text-center">
+                        <span>{data.name || "-"}</span>
+                        <span>{data.position || "-"}</span>
+                        <Link href={route("member", { userSlug: userId })}>
+                            مشاهده پروفایل
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
