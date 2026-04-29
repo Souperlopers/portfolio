3f158cc3aa90ed77f864cedd9947f39664f847b0
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-29 14:11:26 +0330

    ADD EmptyProjects component

---
+++ resources/js/Pages/Home/components/projects/EmptyProjects.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmptyProjects = () => {
+    return (
+        <div>
+            فعلا پروژه ای وجود ندارد.
+        </div>
+    );
+}
+
+export default EmptyProjects;
