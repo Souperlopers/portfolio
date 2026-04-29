bd982c76ada418647d5a13778dab401b13f81221
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-29 12:38:14 +0330

    ADD Banner component to homepage

--- resources/js/Pages/Home/Home.tsx
+++ resources/js/Pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { Link, Head } from "@inertiajs/react";
 import MainLayout from "@/Layouts/MainLayout";
+import { Banner } from "../index";
 
 export default function Home({
     projects,
@@ -13,7 +14,9 @@ export default function Home({
         <div>
             <Head title="SouperLopers" />
             <div className="flex flex-col gap-5">
-                <div className="rounded bg-yellow-200 h-52">Banner</div>
+                <div className="h-52">
+                    <Banner />
+                </div>
                 <div className="bg-yellow-400 rounded h-64">Projects</div>
                 <div className="rounded bg-orange-400 h-48">Members</div>
             </div>
