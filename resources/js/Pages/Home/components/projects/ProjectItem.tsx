046a1fd4131922bd9280ef931e0c92096c144ac5
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-29 14:33:21 +0330

    REMOVE fake data from ProjectItem

--- resources/js/Pages/Home/components/projects/ProjectItem.tsx
+++ resources/js/Pages/Home/components/projects/ProjectItem.tsx
@@ -2,16 +2,14 @@ import { Link } from "@inertiajs/react";
 import { Project } from "@/types";
 
 const ProjectItem = ({ data }: { data: Project }) => {
-    const tagsCount = data.tags.length || 0;
-    const tags = data.tags || [];
     const projectId = data.id || 0;
 
-    const checkIsLastItem = (index: number) => index === tagsCount - 1;
+    // const checkIsLastItem = (index: number) => index === tagsCount - 1;
 
     return (
         <div className="w-full flex justify-between bg-white p-3 rounded">
             <div>{data?.name || "-"}</div>
-            <div className="text-blue-500" dir="ltr">
+            {/* <div className="text-blue-500" dir="ltr">
                 {tags.map((tag, index) => {
                     let isLastItem = checkIsLastItem(index);
                     return (
@@ -21,7 +19,7 @@ const ProjectItem = ({ data }: { data: Project }) => {
                         </span>
                     );
                 })}
-            </div>
+            </div> */}
             <Link href={route("project", { projectSlug: data.id })}>
                 <div className="cursor-pointer">⬅️</div>
             </Link>
