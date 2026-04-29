dd1399f2a4d412470905169bdda01827902fe1a8
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-29 14:32:17 +0330

    STYLE set rtl direction

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Authenticated({ children }: { children: ReactNode }) {
     return (
-        <div className="p-5">
+        <div className="p-5" dir="rtl">
             {children}
         </div>
     );
