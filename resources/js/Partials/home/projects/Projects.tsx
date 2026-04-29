f4b4a95e326b9bbc579c154cfb76382370475a54
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-29 18:42:51 +0330

    STYLE Projects responsive

--- resources/js/Partials/home/projects/Projects.tsx
+++ resources/js/Partials/home/projects/Projects.tsx
@@ -7,7 +7,7 @@ const Projects = ({projects}:{projects:Project[]}) => {
     const topThreeProjects: Project[] = projects.slice(0, 3);
 
     return (
-        <div className="px-10 bg-yellow-500 rounded py-5 flex flex-col gap-6">
+        <div className="lg:px-10 lg:py-5 px-2 py-3 bg-yellow-500 rounded flex flex-col gap-6">
             <h1> پروژه ها</h1>
             {projectsCount === 0 && <EmptyProjects />}
             {topThreeProjects.map((project) => (
