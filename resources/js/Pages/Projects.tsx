f1ecc4dee20208d5c04ff6e560dd79e57940fb6e
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-29 14:11:01 +0330

    REMOVE Projects page

--- resources/js/Pages/Projects.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-import MainLayout from "@/Layouts/MainLayout";
-
-export default function Projects({ projects }: { projects: object }) {
-    return <></>;
-}
-
-Projects.layout = (page: ReactNode) => <MainLayout children={page} />;
