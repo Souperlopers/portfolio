7889ee1c3de598dfa6f0a7ec689f59b77e5b4174
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-29 14:10:44 +0330

    REMOVE Members page

--- resources/js/Pages/Members.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-import MainLayout from "@/Layouts/MainLayout";
-
-export default function Members({ members }: { members: object }) {
-    return <></>;
-}
-
-Members.layout = (page: ReactNode) => <MainLayout children={page} />;
