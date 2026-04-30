b73c88a441d944e9d5928bae93d89f746a6481d4
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-04-30 01:15:15 -0700

    ADD member resources

---
+++ app/Http/Resources/Member/MemberBriefResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Member;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MemberBriefResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => (int) $this->id,
+            'name' => $this->name,
+            'position' => $this->position,
+            'url' => request()->schemeAndHttpHost() . '/api/members/' . $this->slug,
+            'priority' => (int) isset($this->pivot->member_priority_in_project)
+                ? $this->pivot->member_priority_in_project
+                : $this->priority,
+        ];
+    }
+}
