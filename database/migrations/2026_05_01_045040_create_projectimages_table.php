d4e90cc7d1f43d417ecece33d78b64372f247eb2
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-05-01 15:58:53 -0700

    feat(ProjectimageMigration): add projectimage migration

---
+++ database/migrations/2026_05_01_045040_create_projectimages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projectimages', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('project_id')->unsigned();
+            $table->tinyInteger('priority')->default(0)->index();
+            $table->string('path')/*->unique()*/;
+
+            $table->foreign('project_id')->references('id')->on('projects')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projectimages');
+    }
+};
