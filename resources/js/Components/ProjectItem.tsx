c29d64cc16a254efb3d6c7c9f50cbcdadfa2f176
Zahra Rostami <zahrarostami137878@gmail.com>
2026-05-01 18:47:55 +0330

    test todo extention

--- resources/js/Components/ProjectItem.tsx
+++ resources/js/Components/ProjectItem.tsx
@@ -9,6 +9,7 @@ const ProjectItem = ({ projectData }: { projectData: ProjectBrief }) => {
         <div className="w-full flex gap-5 bg-white p-1 rounded">
             <div className="bg-orange-300 w-1/5 h-48 rounded">
                 <img src={projectData.thumbnail} alt={`${projectName} cover`} />
+                {/* TODO_Z HELLO */}
             </div>
             <div className="flex flex-col gap-5 w-4/5 py-3">
                 <div className="truncate max-w-32 sm:max-w-xs lg:max-w-sm xl:max-w-md">
