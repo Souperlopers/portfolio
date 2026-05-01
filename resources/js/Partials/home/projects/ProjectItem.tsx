7de9a6bd86b1e9c9d4d4bce2d0f7c65d1451ca05
Zahra Rostami <zahrarostami137878@gmail.com>
2026-05-01 12:24:34 +0330

    style(projects): add imge and set 3 col style

--- resources/js/Partials/home/projects/ProjectItem.tsx
+++ resources/js/Partials/home/projects/ProjectItem.tsx
@@ -6,12 +6,22 @@ const ProjectItem = ({ data }: { data: Project }) => {
     const projectName = data?.title || "بدون نام";
 
     return (
-        <div className="w-full flex justify-between bg-white p-3 rounded">
-            <div className="truncate max-w-32 sm:max-w-xs lg:max-w-sm xl:max-w-md">
-                {projectName}
+        <div className="w-full flex gap-5 bg-white p-1 rounded">
+            <div className="bg-orange-300 w-1/5 h-48 rounded"></div>
+            <div className="flex flex-col gap-5 w-4/5 py-3">
+                <div className="truncate max-w-32 sm:max-w-xs lg:max-w-sm xl:max-w-md">
+                    {projectName}
+                </div>
+                <div>descriptions</div>
+                <div>tags</div>
             </div>
-            <Link href={route("project", { slug: projectId })}>
-                <div className="cursor-pointer">⬅️</div>
+            <Link
+                className="flex justify-end items-end w-1/12"
+                href={route("project", { slug: projectId })}
+            >
+                <button className="cursor-pointer bg-green-400 px-5 py-3 rounded">
+                    Details
+                </button>
             </Link>
         </div>
     );
--- resources/js/Partials/home/projects/Projects.tsx
+++ resources/js/Partials/home/projects/Projects.tsx
@@ -2,13 +2,18 @@ import ProjectItem from "./ProjectItem";
 import EmptyProjects from "./EmptyProjects";
 import { Project } from "@/types";
 
-const Projects = ({projects}:{projects:Project[]}) => {
+const Projects = ({ projects }: { projects: Project[] }) => {
     const projectsCount = projects.length;
     const topThreeProjects: Project[] = projects.slice(0, 3);
 
     return (
         <div className="lg:px-10 lg:py-5 px-2 py-3 bg-yellow-500 rounded flex flex-col gap-6">
-            <h1 className=""> پروژه ها</h1>
+            <div className="flex justify-center gap-10">
+                <p className="border-b border-neutral-800">همه پروژه ها</p>
+                <p>فروشگاهی</p>
+                <p>شخصی</p>
+                <p>مدیریتی</p>
+            </div>
             {projectsCount === 0 && <EmptyProjects />}
             {topThreeProjects.map((project) => (
                 <ProjectItem data={project} />
