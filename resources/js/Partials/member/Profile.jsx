4abdc8461a6cb511425c5451d7c8aed6296a270f
Zahra Rostami <zahrarostami137878@gmail.com>
2026-05-02 11:27:58 +0330

    update(Profile)

--- resources/js/Partials/member/Profile.jsx
+++ resources/js/Partials/member/Profile.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
-const Profile = ({ info }) => {
-    console.log(info);
-
+const Profile = ({info}) => {
     return (
-        <div className='flex justify-between h-48 w-full rounded bg-yellow-200'>
+        <div className='flex justify-between h-48 w-full rounded bg-black'>
             <div className='w-48 h-48 bg-pink-800 rounded-full'>
                 Image
             </div>
@@ -13,6 +11,6 @@ const Profile = ({ info }) => {
             </div>
         </div>
     );
-}
+};
 
 export default Profile;
