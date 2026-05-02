e9b074de3dc64543bf9bc9cb9aa98d289969806d
Zahra Rostami <zahrarostami137878@gmail.com>
2026-05-02 11:14:59 +0330

    feat(Profile): define Profile component

--- resources/js/Partials/member/Profile.jsx
+++ resources/js/Partials/member/Profile.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Profile = () => {
+const Profile = ({ info }) => {
+    console.log(info);
+
     return (
-        <div>
-            INFO
+        <div className='flex justify-between h-48 w-full rounded bg-yellow-200'>
+            <div className='w-48 h-48 bg-pink-800 rounded-full'>
+                Image
+            </div>
+            <div className='w-2/3 bg-red-100'>
+                Info
+            </div>
         </div>
     );
 }
