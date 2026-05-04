c94ce97b4724cd0bb551807cca428615a088c6a8
Zahra Rostami <zahrarostami137878@gmail.com>
2026-05-04 20:25:13 +0330

    update(ProjectPage): add Info, Images and Contributors sections

--- resources/js/Pages/Project.tsx
+++ resources/js/Pages/Project.tsx
@@ -1,9 +1,27 @@
 import { ReactNode } from "react";
 import MainLayout from "@/Layouts/MainLayout";
 import { ProjectPageProps } from "@/types";
+import { Head } from "@inertiajs/react";
+import {Info, Images, Contributors} from "@/Pages/index";
 
 export default function Project({ project }: ProjectPageProps) {
-    return <></>;
+    const projectData = project.data;
+    const contributors = projectData.contributors;
+
+    return (
+        <div className="flex flex-col gap-5">
+            <Head title="SouperLopers" />
+            <div className="w-full flex flex-col gap-5">
+                <div className="flex justify-between rounded bg-yellow-300">
+                    <Info info={projectData} />
+                    <Images images={projectData.images} />
+                </div>
+                <div className="bg-orange-400 p-5 rounded">
+                    <Contributors contributors={contributors} />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 Project.layout = (page: ReactNode) => <MainLayout children={page} />;
