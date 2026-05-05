79b51eb506bc05e942955cf8ebc0c73b06f9216a
Zahra Rostami <zahrarostami137878@gmail.com>
2026-05-05 19:38:45 +0330

    style(MainLayout): set dark bg

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Authenticated({ children }: { children: ReactNode }) {
     return (
-        <div className="lg:p-5 p-2 bg-black" dir="rtl">
+        <div className="lg:p-5 p-2 bg-neutral-800" dir="rtl">
             {children}
         </div>
     );
