0023265baa058cac176c53857425e0d9425d49cf
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-05-04 21:59:55 -0700

    feat(seeders and models): add limited contribution possibility

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,8 @@ use App\Models\Projectimage;
 use App\Models\Tag;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class DatabaseSeeder extends Seeder
 {
@@ -33,5 +35,33 @@ class DatabaseSeeder extends Seeder
         $tags = (new TagSeeder)->run($tagsQuantity);
         $members = (new MemberSeeder)->run($membersQuantity, $tags);
         $projects = (new ProjectSeeder)->run($projectsQuantity, $members);
+    }
+
+
+    public static function fakePriority(Model $priorable)
+    {
+        return fake()->randomElement([
+            fake()->numberBetween(-128, 127),
+            $priorable->priority
+        ]);
+    }
+
+    public static function random(Collection $collection)
+    {
+        $num = $collection->count();
+        return $collection->random(rand(1, $num));
+    }
+
+    public static function appendableTag(Collection $tags)
+    {
+        $attachmentData = [];
+
+        foreach ($tags as $tag) {
+            $attachmentData[$tag->id] = [
+                'priority_for_taggable' => DatabaseSeeder::fakePriority($tag)
+            ];
+        }
+
+        return $attachmentData;
     }
 }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,65 +2,85 @@
 
 namespace Database\Seeders;
 
+use App\Models\MemberProject;
 use App\Models\Project;
 use App\Models\Projectimage;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(int $projectsQuantity, $members)
+    public function run(int $projectsQuantity, Collection $members)
     {
-        return Project::factory($projectsQuantity)
-            ->create()
-            ->each(function ($project) use ($members) {
-                // Get random members to attach to the project
-                $randomMembers = $members->random(rand(1, $members->count()));
-
-                // Prepare data for attaching members with pivot attributes
-                $attachmentData = [];
-                foreach ($randomMembers as $member) {
-                    $attachmentData[$member->id] = [
-                        'member_priority_in_project' => fake()->randomElement([
-                            fake()->numberBetween(-128, 127),
-                            $member->priority
-                        ]),
-                        'project_priority_for_member' => fake()->randomElement([
-                            fake()->numberBetween(-128, 127),
-                            $project->priority
-                        ]),
-                    ];
-                }
-                $project->members()->attach($attachmentData); // Attach members with additional pivot data
+        return Project::factory($projectsQuantity)->create()->each(
+            function ($project) use ($members) {
+                $this->appendImages($project);
+                $this->appendMembers($project, $members);
+                $this->appendMemberProjectTags($project);
+                $this->appendProjectTags($project);
+            }
+        );
+    }
 
 
+    private function appendImages(Project $project)
+    {
+        $imageNum = fake()->numberBetween(0, 10);
 
-                // Collect all unique tag IDs from the project's members
-                $projectTags = collect();
-                foreach ($project->members as $projectMember) {
-                    $projectTags = $projectTags->merge($projectMember->tags);
-                }
-                $projectTags = $projectTags->unique()->values();
-
-                $attachmentData = [];
-                foreach ($projectTags as $projectTag) {
-                    $attachmentData[$projectTag->id] = [
-                        'priority_for_taggable' => fake()->randomElement([
-                            fake()->numberBetween(-128, 127),
-                            $projectTag->priority
-                        ]),
-                    ];
-                }
-                $project->tags()->attach($attachmentData); // Make the tag IDs unique and attach them to the project
+        $project->image = Projectimage::factory($imageNum)->for($project)->create();
+    }
 
 
+    private function appendMembers(Project $project, Collection $members)
+    {
+        $attachmentData = [];
 
-                // attach projectimages to the project
-                $project->projectimages = Projectimage::factory(
-                    fake()->numberBetween(0, 10)
-                )->for($project)->create();
-            });
+        foreach (DatabaseSeeder::random($members) as $member) {
+            $attachmentData[$member->id] = [
+                'member_priority_in_project' => DatabaseSeeder::fakePriority($member),
+                'project_priority_for_member' => DatabaseSeeder::fakePriority($project)
+            ];
+        }
+
+        $project->members()->attach($attachmentData);
+    }
+
+
+    private function appendMemberProjectTags(Project $project)
+    {
+        foreach ($project->members as $member) {
+            $mp = MemberProject::getRecord($member, $project);
+
+            $tags = $member->tags()->get();
+            $tags = DatabaseSeeder::random($tags);
+            $tags = DatabaseSeeder::appendableTag($tags);
+            $mp->tags()->attach($tags);
+        }
+    }
+
+
+    public function projectMembersTags(Project $project)
+    {
+        $memberProjects = MemberProject::query()->where('project_id', $project->id)->get();
+
+        $tags = collect();
+
+        foreach ($memberProjects as $memberProject) {
+
+            $tags = $tags->merge($memberProject->tags);
+        }
+
+        return $tags->unique()->values();
+    }
+
+
+    private function appendProjectTags(Project $project)
+    {
+        $tags = $this->projectMembersTags($project);
+        $tags = DatabaseSeeder::appendableTag($tags);
+        $project->tags()->attach($tags);
     }
 }
