a8a8b6ab874bda8884c4cbb42f76b07a453a8a68
Zahra Rostami <zahrarostami137878@gmail.com>
2026-05-05 12:02:03 +0330

    update(scrollbar): add title

--- resources/js/Partials/home/ScrollBar.tsx
+++ resources/js/Partials/home/ScrollBar.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import { useState } from "react";
 
 const ScrollBar = () => {
+    const [isDisplayProjectTitle, setIsDisplayProjectTitle] = useState(false);
+    const [isDisplayMemberTitle, setIsDisplayMemberTitle] = useState(false);
+
     const scrollToSection = (id: string) => {
         const element = document.getElementById(id);
         if (!element) return;
@@ -16,14 +19,28 @@ const ScrollBar = () => {
             <div
                 className="hover:bg-red-300 transition-all duration-300 w-full h-full rounded-t-full flex justify-center items-center"
                 onClick={() => scrollToSection("projects")}
+                onMouseEnter={() => setIsDisplayProjectTitle(true)}
+                onMouseLeave={() => setIsDisplayProjectTitle(false)}
             >
                 P
+                {isDisplayProjectTitle && (
+                    <div className="absolute right-16 bg-neutral-500/60 w-16 py-1 text-center rounded">
+                        پروژه ها
+                    </div>
+                )}
             </div>
             <div
                 className="hover:bg-red-300 transition-all duration-300 w-full h-full rounded-b-full flex justify-center items-center"
                 onClick={() => scrollToSection("members")}
+                onMouseEnter={() => setIsDisplayMemberTitle(true)}
+                onMouseLeave={() => setIsDisplayMemberTitle(false)}
             >
                 M
+                {isDisplayMemberTitle && (
+                    <div className="absolute right-16 bg-neutral-500/60 w-16 py-1 text-center rounded">
+                        کاربران
+                    </div>
+                )}
             </div>
         </div>
     );
