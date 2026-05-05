e747afd6a85f6fc84d880ba082eda1b6886725ea
Zahra Rostami <zahrarostami137878@gmail.com>
2026-05-05 11:06:47 +0330

    update(ProjectPage): fix project thumbnail size

--- resources/js/Partials/project/ProjectImages.tsx
+++ resources/js/Partials/project/ProjectImages.tsx
@@ -4,7 +4,7 @@ const ProjectImages = ({ images }:{images: ProjectImagesProps[]}) => {
     return (
         <div className="w-1/2 h-72 border border-neutral-700 rounded relative">
             {images.map((img) => (
-                <img src={img.url} alt="" className="cover"/>
+                <img src={img.url} alt="" className="cover w-full h-full rounded"/>
             ))}
             <div className="absolute bottom-1 right-[40%] flex gap-5">
                 <span className="border border-neutral-800">PREVIOUS</span>
