83843fa4c871469dd62916b246218677263562d3
Zahra Rostami <zahrarostami137878@gmail.com>
2026-05-06 15:20:04 +0330

    update(memberProjects): add type and key

--- resources/js/Partials/member/MemberProjects.tsx
+++ resources/js/Partials/member/MemberProjects.tsx
@@ -1,18 +1,18 @@
 import ProjectItem from "@/Components/ProjectItem";
 import EmptyProjects from "@/Components/EmptyProjects";
+import { ProjectBrief } from "@/types/project";
 
-const MemberProjects = ({ projects }) => {
+const MemberProjects = ({ projects }: { projects: ProjectBrief[] }) => {
     const projectsCount = projects.length;
 
     return (
-        <div className="lg:px-10 lg:py-5 px-2 py-3 bg-yellow-500 rounded flex flex-col gap-6">
+        <div className="py-3 rounded flex flex-col gap-6">
             {projectsCount === 0 && <EmptyProjects />}
             {projects.map((project) => (
-                <ProjectItem projectData={project} />
+                <ProjectItem key={project.id} projectData={project} />
             ))}
         </div>
     );
 };
-
 
 export default MemberProjects;
