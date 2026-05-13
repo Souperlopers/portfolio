950a32897714dbf618db9e76fb994cea7a908263
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-05-14 00:51:40 +0330

    fix(Pages/Project.tsx): add project title

--- resources/js/Pages/Project.tsx
+++ resources/js/Pages/Project.tsx
@@ -6,15 +6,16 @@ import { Info, Images, Contributors } from "@/index";
 
 export default function Project({ project }: ProjectPageProps) {
     const projectData = project.data;
+    const images = projectData.images;
     const contributors = projectData.contributors;
 
     return (
         <div className="flex flex-col gap-5">
-            <Head title="SouperLopers" />
+            <Head title={`SouperLopers ${projectData.title}`} />
             <div className="w-full flex flex-col gap-5">
                 <div className="flex justify-between rounded bg-yellow-300">
                     <Info info={projectData} />
-                    <Images images={projectData.images} />
+                    <Images images={images} />
                 </div>
                 <div className="bg-orange-400 p-5 rounded">
                     <Contributors contributors={contributors} />
