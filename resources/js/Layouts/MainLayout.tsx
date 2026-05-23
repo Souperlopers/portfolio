f3b5ceb1a682dbbb2ca7eb31d54f6f6de4e275a6
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-05-23 23:12:38 +0330

    fix(app.css, MainLayout.tsx): fulscreen style

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -3,11 +3,11 @@ import { ReactNode } from "react";
 export default function MainLayout({ children }: { children: ReactNode }) {
     return (
         <div
-            className="
-        bg-neutral-800 font-vazir
-        flex flex-col gap-5
+        className="
+        min-h-screen bg-neutral-800 font-vazir
+        flex flex-col justify-start items-center
         "
-            dir="rtl"
+        dir="rtl"
         >
             {children}
         </div>
