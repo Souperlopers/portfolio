a842fe2551b4e2df98d59db4e52f6c52cf4a5982
Zahra Rostami <zahrarostami137878@gmail.com>
2026-05-24 18:09:31 +0330

    update(ProjectSection): change padding

--- resources/js/Partials/home/Projects.tsx
+++ resources/js/Partials/home/Projects.tsx
@@ -7,7 +7,7 @@ const Projects = ({ projects }: { projects: ProjectBrief[] }) => {
     const topThreeProjects = projects.slice(0, 3);
 
     return (
-        <section id="projects" className="sm:px-10 py-14 w-full">
+        <section id="projects" className="sm:px-10 lg:p-5 py-14 w-full">
             <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between pb-5">
                 <h2 className="text-2xl sm:text-3xl font-extrabold text-white">
                     پروژه‌ها
