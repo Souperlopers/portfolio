d269f4b8b0a359310bde1331045621736038d63f
Zahra Rostami <zahrarostami137878@gmail.com>
2026-05-24 19:23:06 +0330

    style(): decrese bottom padding

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -5,7 +5,7 @@ export default function MainLayout({ children }: { children: ReactNode }) {
         <div
         className="
         min-h-screen bg-neutral-800 font-vazir
-        flex flex-col justify-start items-center pb-10
+        flex flex-col justify-start items-center pb-5
         "
         dir="rtl"
         >
