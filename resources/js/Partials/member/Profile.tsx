50661c0781d090ea3252584fad0b9af25744066a
Zahra Rostami <zahrarostami137878@gmail.com>
2026-05-24 13:35:01 +0330

    style(Profile): set fix width to mobile mode

--- resources/js/Partials/member/Profile.tsx
+++ resources/js/Partials/member/Profile.tsx
@@ -5,19 +5,19 @@ const Profile = ({ info }: { info: Member }) => {
     const [imgLoaded, setImgLoaded] = useState(false);
     return (
         <div className="flex justify-between gap-3 w-full rounded">
-            <div className="w-1/6">
+            <div className="lg:w-1/6 w-1/3">
                 {!imgLoaded && (
                     <div className="lg:w-48 lg:h-48 md:w-full md:h-42 sm:w-24 sm:h-32 w-20 h-28 bg-neutral-500 rounded-full" />
                 )}
                 <img
                     src={info.thumbnail}
                     alt={`${info.name} cover`}
-                    className="rounded-full w-48 h-48 bg-cover"
+                    className="rounded-full lg:w-48 lg:h-48 w-28 h-28 bg-cover"
                     onLoad={() => setImgLoaded(true)}
                     style={{ display: imgLoaded ? "block" : "none" }}
                 />
             </div>
-            <div className="flex flex-col gap-5 w-5/6 py-3 bg-pink-200">
+            <div className="flex flex-col gap-5 lg:w-5/6 w-2/3 py-3 bg-pink-200">
                 <span>{info.name}</span>
                 <span>{info.position}</span>
             </div>
