0d6a44b7df94cd2ec6702d63dff5acce369a0a5c
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-11 21:20:09 +0330

    style(): add daisyui class

--- resources/js/Components/TagsComponent.tsx
+++ resources/js/Components/TagsComponent.tsx
@@ -4,10 +4,7 @@ const TagsComponent = ({ tags }: { tags: Tag[] }) => {
     return (
         <div className="flex flex-wrap gap-2 min-h-8">
             {tags.map((tag) => (
-                <div
-                    key={tag.id}
-                    className="px-3 py-1.5 rounded-full text-sm font-semibold bg-white/10 text-white/90 ring-1 ring-white/10"
-                >
+                <div className="badge badge-soft badge-md" key={tag.id}>
                     <p className="truncate max-w-16 text-center">{tag.title}</p>
                 </div>
             ))}
