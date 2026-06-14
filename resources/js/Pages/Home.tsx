3f754177b268fc55905744676413980d80303835
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-14 12:40:36 +0330

    update(HomePage): add fake loading with timer

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,10 +1,20 @@
-import { ReactNode } from "react";
+import { ReactNode, useState, useEffect } from "react";
 import { Head } from "@inertiajs/react";
 import MainLayout from "@/Layouts/MainLayout";
-import { Banner, Projects, Members, ScrollBar } from "@/index";
+import { Banner, Projects, Members, ScrollBar, Loading } from "@/index";
 import { HomePageProps } from "@/types";
 
 export default function Home({ projects, members }: HomePageProps) {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const interval = setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+        return () => clearTimeout(interval);
+    }, []);
+
+    if(loading) return <Loading />;
+
     return (
         <>
             <Head title="SouperLopers" />
