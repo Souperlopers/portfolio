7692c8172414376c9145547d5c661225238da27a
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-14 11:36:48 +0330

    style(): change btn bg color

--- resources/js/Partials/project/DetailButtons.tsx
+++ resources/js/Partials/project/DetailButtons.tsx
@@ -1,9 +1,7 @@
 import { ProjectLinks } from "@/types/project";
 import { Link } from "@inertiajs/react";
-import { log } from "console";
 
 const DetailButtons = ({ links }: { links: ProjectLinks }) => {
-    console.log(links);
     
     const linkLoop = Object.entries(links).filter(
         (link: string[]) => link[1] !== null,
@@ -13,7 +11,7 @@ const DetailButtons = ({ links }: { links: ProjectLinks }) => {
         <>
             {linkLoop.map((link) => (
                 <Link key={link[0]} href={link[1]}>
-                    <button className={`bg-neutral-500 rounded w-32 px-3 py-2 font-semibold`}>
+                    <button className={`bg-sky-500 rounded w-32 px-3 py-2 font-semibold`}>
                         <span>{link[0]}</span>
                     </button>
                 </Link>
