160fd4321c6d9ea27c26c480aaa439409d9aeab3
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-14 11:36:15 +0330

    style(): set fix height

--- resources/js/Components/Header.tsx
+++ resources/js/Components/Header.tsx
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "@inertiajs/react";
 
 const Header = () => {
     const [imgLoaded, setImgLoaded] = useState(false);
     return (
-        <div className="px-5 py-2 w-full bg-secondary text-text-primary flex justify-between items-center">
+        <div className="px-5 py-2 w-full h-[75px] bg-secondary text-text-primary flex justify-between items-center">
             <Link href="/" className="flex gap-10">
-                {!imgLoaded && <div className="bg-neutral-200" />}
+                {!imgLoaded && <div className="bg-stone-400 h-16 w-24 rounded"></div>}
                 <img
                     src={`/assets/images/logo.svg`}
-                    alt="logo"
-                    className="object-center"
+                    alt="SouperLopers"
+                    className="object-cover w-full h-full"
                     onLoad={() => setImgLoaded(true)}
                     style={{ display: imgLoaded ? "block" : "none" }}
                 />
             </Link>
             <div>
                 <ul className="flex gap-5 flex-row-reverse">
-                    <li>Home</li>
+                    <Link href="/">Home</Link>
                     <li>Projects</li>
                     <li>Members</li>
                 </ul>
