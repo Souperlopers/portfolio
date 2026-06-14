74b9a6b43f3a973569f4611e4b093b658c7fc074
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-14 12:37:49 +0330

    update(index): add Laoding

--- resources/js/index.tsx
+++ resources/js/index.tsx
@@ -1,13 +1,15 @@
 export { default as Banner } from "@/Partials/home/Banner";
 export { default as Projects } from "@/Partials/home/Projects";
 export { default as Members } from "@/Partials/home/Members";
-export { default as ScrollBar } from "@/Partials/home/ScrollBar"; 
+export { default as ScrollBar } from "@/Partials/home/ScrollBar";
 
 export { default as Profile } from "@/Partials/member/Profile";
 export { default as MemberProjects } from "@/Partials/member/MemberProjects";
-export {default as Contact} from "@/Partials/member/ContactMember"
+export { default as Contact } from "@/Partials/member/ContactMember";
 
 export { default as Info } from "@/Partials/project/ProjectInfo";
 export { default as DetailButtons } from "@/Partials/project/DetailButtons";
 export { default as Images } from "@/Partials/project/ProjectImages";
 export { default as Contributors } from "@/Partials/project/ProjectMembers";
+
+export { default as Loading } from "@/Components/Loading";
