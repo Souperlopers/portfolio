98917c057a1fa825cbbe4a251a43b8726e1fb22b
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-15 19:45:20 +0330

    style(TagComponents): set fix height and ltr direction for tags

--- resources/js/Components/TagsComponent.tsx
+++ resources/js/Components/TagsComponent.tsx
@@ -2,10 +2,10 @@ import { Tag } from "@/types/tag";
 
 const TagsComponent = ({ tags }: { tags: Tag[] }) => {
     return (
-        <div className="flex flex-wrap gap-2 min-h-8">
+        <div className="flex flex-wrap gap-2 h-8 overflow-hidden">
             {tags.map((tag) => (
-                <div className="badge badge-soft badge-md" key={tag.id}>
-                    <p className="truncate max-w-16 text-center">{tag.title}</p>
+                <div className="badge badge-soft badge-md" key={tag.id} dir="ltr">
+                    <p className="truncate max-w-20 text-center">{tag.title}</p>
                 </div>
             ))}
         </div>
