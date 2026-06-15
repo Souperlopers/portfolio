33ddba9f77e4ab44a421c66784b988974580757f
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-15 19:45:57 +0330

    style(Banner): change height

--- resources/js/Partials/home/Banner.tsx
+++ resources/js/Partials/home/Banner.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const Banner = ({ fileName }: { fileName: string }) => {
     const [imgLoaded, setImgLoaded] = useState(false);
     return (
-        <div className="w-full h-20 sm:h-64 md:h-80 lg:h-96 relative rounded">
+        <div className="w-full h-36 sm:h-48 md:h-64 lg:h-96 relative rounded">
             <div className="flex justify-end lg:w-full lg:h-full md:w-full md:h-42 w-full overflow-hidden">
                 {!imgLoaded && (
                     <div className="w-full h-full bg-neutral-300 animate-pulse absolute inset-0" />
