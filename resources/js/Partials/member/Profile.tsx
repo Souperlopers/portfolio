61b06c17d814fc68793b9c668e0c68b24bf4cc6d
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-16 18:30:25 +0330

    style(Profile): responsive

--- resources/js/Partials/member/Profile.tsx
+++ resources/js/Partials/member/Profile.tsx
@@ -2,33 +2,39 @@ import { useState } from "react";
 import { Member } from "@/types/member";
 
 const Profile = ({ info }: { info: Member }) => {
+    console.log(info);
+    
     const [imgLoaded, setImgLoaded] = useState(false);
     return (
-        <div className="flex flex-col justify-between gap-3 w-full rounded">
-            <div className="w-full h-48 bg-stone-400 rounded relative">
-                <div className="lg:w-1/6 w-full flex justify-center absolute top-20">
+        <div className="flex flex-col justify-between md:gap-32 gap-20 w-full rounded">
+            {/* banner, image */}
+            <div className="w-full h-56 bg-stone-400 rounded relative avatar">
+                <div className="md:w-48 md:h-48 w-32 h-32 flex justify-center absolute md:top-32 top-40 md:right-5 right-2 rounded-full border-4 border-b-sky-600 overflow-hidden">
                     {!imgLoaded && (
                         <div className="lg:w-48 lg:h-48 md:w-full md:h-42 sm:w-24 sm:h-32 w-20 h-28 bg-neutral-500 rounded-full" />
                     )}
                     <img
                         src={info.thumbnail}
                         alt={`${info.name} cover`}
-                        className="rounded-full w-48 h-48 object-cover"
+                        className="w-full h-full object-cover"
                         onLoad={() => setImgLoaded(true)}
                         style={{ display: imgLoaded ? "block" : "none" }}
                     />
                 </div>
             </div>
-            <div className="flex flex-col justify-between lg:w-5/6 w-full border border-white/40 rounded-xl p-5 shadow-lg shadow-primary/10 mt-24">
-                <div className="flex flex-col gap-1">
-                    <span className="text-text-primary text-3xl font-bold">
-                        {info.name}
+            <div className="flex flex-col justify-between gap-5 w-full px-5">
+                <div className="flex flex-col gap-3">
+                    {/* name */}
+                    <span className="text-text-primary md:text-4xl text-2xl font-semibold truncate md:max-w-[800px] overflow-hidden">
+                    {info.name}
                     </span>
-                    <span className="text-accent text-base font-semibold">
+                    {/* position */}
+                    <span className="text-stone-400 md:text-base text-sm font-semibold truncate md:max-w-[800px] overflow-hidden">
                         {info.position}
                     </span>
                 </div>
-                <span className="text-white/80 text-lg font-semibold">
+                {/* description */}
+                <span className="text-white/80 md:text-lg text-base font-semibold">
                     {info.description}
                 </span>
             </div>
