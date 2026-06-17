ebe2f295ef81d26098b5cec91f42e91ce5dc28ec
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-17 19:11:24 +0330

    update(Header): add home icon and remove projects and members items

--- resources/js/Components/Header.tsx
+++ resources/js/Components/Header.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { Link } from "@inertiajs/react";
+import { AiFillHome } from "react-icons/ai";
 
 const Header = () => {
     const [imgLoaded, setImgLoaded] = useState(false);
     return (
         <div className="px-5 py-2 w-full h-[75px] bg-secondary text-text-primary flex justify-between items-center">
             <Link href="/" className="flex gap-10">
-                {!imgLoaded && <div className="bg-stone-400 h-16 w-24 rounded"></div>}
+                {!imgLoaded && (
+                    <div className="bg-stone-400 h-16 w-24 rounded"></div>
+                )}
                 <img
                     src={`/assets/images/logo.svg`}
                     alt="SouperLopers"
@@ -15,12 +18,10 @@ const Header = () => {
                     style={{ display: imgLoaded ? "block" : "none" }}
                 />
             </Link>
-            <div>
-                <ul className="flex gap-5 flex-row-reverse">
-                    <Link href="/">Home</Link>
-                    <li>Projects</li>
-                    <li>Members</li>
-                </ul>
+            <div className="hover:scale-105 hover:text-sky-300 duration-300 transition-all">
+                <Link href="/">
+                    <AiFillHome size={32} />
+                </Link>
             </div>
         </div>
     );
