09fc44fb2169bcad9de6a4a7819feb33000d3cff
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-21 22:17:22 +0330

    style(Banner): delete bannerScale animation and change bannerOpacity

--- resources/js/Partials/home/Banner.tsx
+++ resources/js/Partials/home/Banner.tsx
@@ -4,12 +4,11 @@ import BannerImage from "./BannerImage";
 const Banner = () => {
     const { scrollY } = useScroll();
     
-    const bannerOpacity = useTransform(scrollY, [0, 100], [1, 0.6]);
-    const bannerScale = useTransform(scrollY, [0, 100], [1, 0.95]);
+    const bannerOpacity = useTransform(scrollY, [0, 400], [1, 0.1]);
 
     return (
         <motion.div
-            style={{ opacity: bannerOpacity, scale: bannerScale }}
+            style={{ opacity: bannerOpacity }}
             className="relative mx-auto lg:w-[1350px] w-[400px] max-w-full h-[490px] overflow-hidden mb-12"
         >
             <BannerImage />
