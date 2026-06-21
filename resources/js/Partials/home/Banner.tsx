3dffd3d5ff0b09f2ca5406500b7ddf3f11fc68f2
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-21 11:19:42 +0330

    style(Banner): change banner opacity and sclae animation

--- resources/js/Partials/home/Banner.tsx
+++ resources/js/Partials/home/Banner.tsx
@@ -4,13 +4,13 @@ import BannerImage from "./BannerImage";
 const Banner = () => {
     const { scrollY } = useScroll();
     
-    const bannerOpacity = useTransform(scrollY, [0, 500], [1, 0.4]);
-    const bannerScale = useTransform(scrollY, [0, 500], [1, 0.95]);
+    const bannerOpacity = useTransform(scrollY, [0, 100], [1, 0.1]);
+    const bannerScale = useTransform(scrollY, [0, 100], [1, 0.95]);
 
     return (
         <motion.div
             style={{ opacity: bannerOpacity, scale: bannerScale }}
-            className="relative mx-auto w-[1350px] max-w-full h-[490px] overflow-hidden"
+            className="relative mx-auto w-[1350px] max-w-full h-[490px] overflow-hidden mb-12"
         >
             <BannerImage />
         </motion.div>
