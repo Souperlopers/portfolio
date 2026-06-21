342aeadce0c9a9091efd4a5b32087033b9b1175a
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-21 10:04:01 +0330

    style(MainLayout): change bg color

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -5,7 +5,7 @@ export default function MainLayout({ children }: { children: ReactNode }) {
     return (
         <div
         className="
-        min-h-screen bg-neutral-800 font-vazir
+        min-h-screen bg-[#252526] font-vazir
         flex flex-col justify-start items-center pb-5
         "
         dir="rtl"
