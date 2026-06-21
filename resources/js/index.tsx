3071c81bb746ece56cdb2f2dd5a759e5451678f8
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-21 20:01:47 +0330

    update(index): add Logo and remove scrollbar

--- resources/js/index.tsx
+++ resources/js/index.tsx
@@ -1,7 +1,7 @@
 export { default as Banner } from "@/Partials/home/Banner";
 export { default as Projects } from "@/Partials/home/Projects";
 export { default as Members } from "@/Partials/home/Members";
-export { default as ScrollBar } from "@/Partials/home/ScrollBar";
+export { default as Logo } from "@/Components/Logo";
 
 export { default as Profile } from "@/Partials/member/Profile";
 export { default as MemberProjects } from "@/Partials/member/MemberProjects";
