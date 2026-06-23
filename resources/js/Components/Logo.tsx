ced42b7eaa8b04f8f838eaa19b1c5bf2285e1f03
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-23 20:18:39 +0330

    update(Logo): set positions for mobile

--- resources/js/Components/Logo.tsx
+++ resources/js/Components/Logo.tsx
@@ -1,70 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 
 const Logo = () => {
-    const [imagesLoadedCount, setImagesLoadedCount] = useState(0);
+    const [isMobiled, setIsMobiled] = useState(false);
+    const windowWidth = window.innerWidth;
+
+    useEffect(()=>{
+        if(windowWidth < 500) setIsMobiled(true);
+        else setIsMobiled(false);
+    },[windowWidth])
 
     const { scrollY } = useScroll();
-    const souperX = useTransform(scrollY, [0, 300], ["0px", "12px"]);
-    const souperY = useTransform(scrollY, [0, 300], ["0px", "-2px"]);
-    const logoW = useTransform(scrollY, [0, 300], ["320px", "100px"]);
 
-    const lopersX = useTransform(scrollY, [0, 300], ["0px", "-55px"]);
-    const lopersY = useTransform(scrollY, [0, 300], ["0px", "20px"]);
-
-    const logoTop = useTransform(scrollY, [0, 300], ["80px", "20px"]);
-    const logoRight = useTransform(scrollY, [0, 300], ["80%", "0px"]);
-
+    const souperX = useTransform(scrollY, [0, 300], [isMobiled?"545px":"-30px", isMobiled?"850px":"780px"]);
+    const souperY = useTransform(scrollY, [0, 300], [isMobiled?"-250px":"0px", isMobiled?"-320px":"-280px"]);
+    const lopersX = useTransform(scrollY, [0, 300], [isMobiled?"545px":"-30px", isMobiled?"650px":"580px"]);
+    const lopersY = useTransform(scrollY, [0, 300], [isMobiled?"-250px":"0px", isMobiled?"-250px":"-210px"]);
+    const logoScale = useTransform(
+        scrollY,
+        [0, 300],
+        [isMobiled ? 0.36 : 1, isMobiled ? 0.30 : 0.35],
+    );
     const ptxOpacity = useTransform(scrollY, [0, 300], [1, 0]);
 
     return (
-        <>
-            {imagesLoadedCount < 3 && (
-                <div className="bg-[#494949] h-32 rounded skeleton flex lg:w-80 w-40 z-[100] fixed"></div>
-            )}
+        <motion.div
+            style={{ }}
+            className="fixed top-20 right-[5%] z-[100] flex items-center selection:bg-transparent"
+            dir="ltr"
+        >
             <motion.div
                 style={{
-                    top: logoTop,
-                    right: logoRight,
-                    width: logoW,
+                    scale: logoScale,
+                    transformOrigin: "left center",
+                    display: "flex",
                 }}
-                className="fixed z-[100] flex lg:w-80 w-40 selection:bg-transparent "
-                dir="ltr"
             >
                 <motion.div style={{ x: souperX, y: souperY }}>
                     <img
                         src="/assets/images/logo-images/souper.svg"
                         alt="souper"
                         className="drop-shadow-2xl"
-                        style={{
-                            display: imagesLoadedCount === 3 ? "block" : "none",
-                        }}
-                        onLoad={() => setImagesLoadedCount((prev) => prev + 1)}
                     />
                 </motion.div>
                 <motion.div style={{ x: lopersX, y: lopersY }}>
                     <img
                         src="/assets/images/logo-images/lopers.svg"
                         alt="lopers"
-                        style={{
-                            display: imagesLoadedCount === 3 ? "block" : "none",
-                        }}
-                        onLoad={() => setImagesLoadedCount((prev) => prev + 1)}
                     />
                 </motion.div>
                 <motion.div style={{ opacity: ptxOpacity }}>
                     <img
                         src="/assets/images/logo-images/ptx.svg"
                         alt="ptx"
-                        className="absolute lg:-right-5 -right-6 lg:w-9 w-5"
-                        style={{
-                            display: imagesLoadedCount === 3 ? "block" : "none",
-                        }}
-                        onLoad={() => setImagesLoadedCount((prev) => prev + 1)}
+                        className="absolute lg:-right-1 lg:top-0 -right-[575px] -top-[250px] lg:w-8 w-6"
                     />
                 </motion.div>
             </motion.div>
-        </>
+        </motion.div>
     );
 };
 
