0d92099ea37966f2b73b6778fea33d8374f5797d
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-28 12:15:44 +0330

    test smart indent cols

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,8 +1,8 @@
-import { ReactNode, useState, useEffect } from "react";
-import { Head } from "@inertiajs/react";
-import MainLayout from "@/Layouts/MainLayout";
-import { Banner, Projects, Members, Logo, Loading } from "@/index";
-import { HomePageProps } from "@/types";
+import { ReactNode    , useState, useEffect }              from "react"               ;
+import { Head          }                                   from "@inertiajs/react"    ;
+import   MainLayout                                        from "@/Layouts/MainLayout";
+import { Banner       , Projects, Members, Logo, Loading } from "@/index"             ;
+import { HomePageProps }                                   from "@/types"             ;
 
 export default function Home({ projects, members }: HomePageProps) {
     const [loading, setLoading] = useState(true);
@@ -10,7 +10,7 @@ export default function Home({ projects, members }: HomePageProps) {
     useEffect(() => {
         const timer = setTimeout(() => setLoading(false), 1800);
         return () => clearTimeout(timer);
-    }, []);
+        }, []);
 
     if (loading) return <Loading />;
 
