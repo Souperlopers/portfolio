0ad13b9b7dd9fab0410a3bed0631b9789a224ce1
Zahra Rostami <zahrarostami137878@gmail.com>
2026-06-30 18:45:18 +0330

    style(Members): change style type to css file

--- resources/js/Partials/home/Members.tsx
+++ resources/js/Partials/home/Members.tsx
@@ -1,5 +1,6 @@
 import MemberCard from "@/Components/MemberCard";
 import { MemberBrief } from "@/types/member";
+import styles from "./style/MembersStyle.module.css";
 
 const Members = ({ members }: { members: MemberBrief[] }) => {
     return (
@@ -7,7 +8,7 @@ const Members = ({ members }: { members: MemberBrief[] }) => {
             <h2 className="text-xl md:text-2xl lg:text-3xl font-medium text-white mb-5 md:mb-7">
                 توسعه‌دهندگان
             </h2>
-            <div className="flex flex-wrap gap-3 md:gap-4 lg:gap-5">
+            <div className={styles.container}>
                 {members.map((member) => (
                     <MemberCard key={member.id} memberData={member} />
                 ))}
