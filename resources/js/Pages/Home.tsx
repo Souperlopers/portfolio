5fcc235616e5f386dfbb554f749af9a81f97e2b0
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-03 20:29:18 +0330

    update(Banner): scroll-X problem

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,8 +1,9 @@
-import { ReactNode    , useState, useEffect }              from "react"               ;
-import { Head          }                                   from "@inertiajs/react"    ;
-import   MainLayout                                        from "@/Layouts/MainLayout";
-import { Banner       , Projects, Members, Logo, Loading } from "@/index"             ;
-import { HomePageProps }                                   from "@/types"             ;
+import { ReactNode, useState, useEffect } from "react";
+import { Head } from "@inertiajs/react";
+import MainLayout from "@/Layouts/MainLayout";
+import { Banner, Projects, Members, Logo, Loading } from "@/index";
+import { HomePageProps } from "@/types";
+import Subtract1 from "@/Partials/home/Subtract1";
 
 export default function Home({ projects, members }: HomePageProps) {
     const [loading, setLoading] = useState(true);
@@ -10,18 +11,22 @@ export default function Home({ projects, members }: HomePageProps) {
     useEffect(() => {
         const timer = setTimeout(() => setLoading(false), 1800);
         return () => clearTimeout(timer);
-        }, []);
+    }, []);
 
     if (loading) return <Loading />;
 
     return (
-        <div className="w-full max-w-[1350px] mx-auto relative">
-            <Head title="SouperLopers" />
+        <>
             <Banner />
-            <Logo />
-            <Projects projects={projects.data} />
-            <Members members={members.data} />
-        </div>
+
+
+            <div className="w-full max-w-[1343px] relative">
+                <Head title="SouperLopers" />
+                <Logo />
+                <Projects projects={projects.data} />
+                <Members members={members.data} />
+            </div>
+        </>
     );
 }
 
--- resources/js/Partials/home/Banner.tsx
+++ resources/js/Partials/home/Banner.tsx
@@ -1,17 +1,36 @@
 import { motion, useScroll, useTransform } from "framer-motion";
 import BannerImage from "./BannerImage";
+import Subtract1 from "./Subtract1";
 
 const Banner = () => {
     const { scrollY } = useScroll();
-    
+
     const bannerOpacity = useTransform(scrollY, [0, 400], [1, 0.1]);
 
     return (
         <motion.div
             style={{ opacity: bannerOpacity }}
-            className="relative mx-auto lg:w-[1350px] w-[400px] max-w-full overflow-hidden mb-12"
+            className="w-[1350px] h-[500px] relative"
         >
-            <BannerImage />
+            <Subtract1 />
+            <div
+                className=""
+                style={{ aspectRatio: "1360 / 510" }}
+            >
+                <img
+                    src="/assets/images/banner-images/Subtract.svg"
+                    alt="banner bg"
+                    style={{
+                        position: "absolute",
+                        left: "-15%",
+                        top: "-1%",
+                        width: "3000px",
+                        height:"510px",
+                        zIndex: 10,
+                    }}
+                />
+            </div>
+            {/* <BannerImage /> */}
         </motion.div>
     );
 };
