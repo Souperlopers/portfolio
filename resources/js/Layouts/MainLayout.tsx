7677d97fc74f4ec3730bcdbd848407841d277735
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-04 18:35:28 +0330

    update(MainLayout): add Logo and Banner

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -1,15 +1,20 @@
 import { ReactNode } from "react";
 import Header from "@/Components/Header";
+import { Banner, Logo } from "@/index";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
+
     return (
         <div
-        className="min-h-screen overflow-hidden pb-5 
-        bg-[#252526] font-vazir
-        flex flex-col justify-start items-center"
-        dir="rtl"
+            dir="rtl"
+            className="min-h-screen relative
+                    overflow-hidden pb-5 
+                    bg-[#252526] font-vazir
+                    flex flex-col justify-start items-center"
         >
             <Header />
+            <Logo />
+            <Banner />
             {children}
         </div>
     );
