759e7e9450c6cbe79ea8abf012d57fdbc519c2bf
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-04 20:42:59 +0330

    update(MainLayout): remove Logo and add Loading

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -1,21 +1,34 @@
-import { ReactNode } from "react";
-import Header from "@/Components/Header";
-import { Banner, Logo } from "@/index";
+import { useState , useEffect       } from "react"           ;
+import { ReactNode                  } from "react"           ;
+import { router   , Head            } from "@inertiajs/react";
+import { Banner   , Loading, Header } from "@/index"         ;
 
 export default function MainLayout({ children }: { children: ReactNode }) {
+    const [isloading, setIsloading] = useState(false);
+
+    useEffect(() => {
+        const removeStart = router.on("start", () => setIsloading(true));
+        const removeFinish = router.on("finish", () => setIsloading(false));
+
+        return () => {
+            removeStart();
+            removeFinish();
+        };
+    }, []);
 
     return (
         <div
             dir="rtl"
             className="min-h-screen relative
-                    overflow-hidden pb-5 
-                    bg-[#252526] font-vazir
-                    flex flex-col justify-start items-center"
-        >
-            <Header />
-            <Logo />
-            <Banner />
-            {children}
+            overflow-hidden pb-5
+            bg-[#252526] font-vazir
+            flex flex-col justify-start items-center"
+            >
+            <Head      title="SouperLopers" />
+            {isloading && <Loading />}
+            <Header    />
+            <Banner    />
+            {children }
         </div>
     );
 }
