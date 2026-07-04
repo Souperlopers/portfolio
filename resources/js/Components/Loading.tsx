5d229fb6f3d6e3dc49ce774719587cef16f5e981
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-04 18:37:29 +0330

    style(Loading): increase z index

--- resources/js/Components/Loading.tsx
+++ resources/js/Components/Loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
     return (
-        <div className="fixed inset-0 w-full h-dvh flex justify-center items-center bg-stone-950 z-50">
+        <div className="fixed inset-0 w-full h-dvh flex justify-center items-center bg-stone-950 z-[100]">
             <div className="flex flex-col items-center md:gap-5 gap-2">
                 <div className="md:w-48 w-28 overflow-hidden">
                     <img
