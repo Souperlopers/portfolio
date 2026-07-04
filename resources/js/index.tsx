70188cb91044ac4d132d95ad88c7b8cff06e3945
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-04 18:36:44 +0330

    update(index): add Header

--- resources/js/index.tsx
+++ resources/js/index.tsx
@@ -2,6 +2,7 @@ export { default as Banner } from "@/Partials/home/Banner";
 export { default as Projects } from "@/Partials/home/Projects";
 export { default as Members } from "@/Partials/home/Members";
 export { default as Logo } from "@/Components/Logo";
+export {default as Header} from "@/Components/Header";
 
 export { default as Profile } from "@/Partials/member/Profile";
 export { default as MemberProjects } from "@/Partials/member/MemberProjects";
