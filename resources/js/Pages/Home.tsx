d5fb2a30989a5e100d3d0fdac4e2abb70e4e1ddc
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-05 19:21:37 +0330

    update(Home): add Banner

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,11 +1,12 @@
-import { ReactNode } from "react";
-import MainLayout from "@/Layouts/MainLayout";
-import { Projects, Members } from "@/index";
-import { HomePageProps } from "@/types";
+import { ReactNode     }                   from "react"               ;
+import   MainLayout                        from "@/Layouts/MainLayout";
+import { Banner       ,Projects, Members } from "@/index"             ;
+import { HomePageProps }                   from "@/types"             ;
 
 export default function Home({ projects, members }: HomePageProps) {
     return (
         <>
+            <Banner />
             <Projects projects={projects.data} />
             <Members members={members.data} />
         </>
