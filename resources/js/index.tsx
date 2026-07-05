c90ed6a06955e7ff6053d8b47b1ab692d66f5ce2
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-05 19:14:35 +0330

    update(index): add Logo, Header and DrawerMenu

--- resources/js/index.tsx
+++ resources/js/index.tsx
@@ -1,16 +1,19 @@
-export { default as Banner } from "@/Partials/home/Banner";
-export { default as Projects } from "@/Partials/home/Projects";
-export { default as Members } from "@/Partials/home/Members";
-export { default as Logo } from "@/Components/Logo";
-export { default as MainHeader } from "@/Components/MainHeader";
+export { default as Banner     } from "@/Partials/home/Banner"        ;
+export { default as Projects   } from "@/Partials/home/Projects"      ;
+export { default as Members    } from "@/Partials/home/Members"       ;
+export { default as MainLogo   } from "@/Components/logo/MainLogo"    ;
+export { default as MainHeader } from "@/Components/header/MainHeader";
 
-export { default as Profile } from "@/Partials/member/Profile";
+export { default as Profile        } from "@/Partials/member/Profile"       ;
 export { default as MemberProjects } from "@/Partials/member/MemberProjects";
-export { default as Contact } from "@/Partials/member/ContactMember";
+export { default as Contact        } from "@/Partials/member/ContactMember" ;
 
-export { default as Info } from "@/Partials/project/ProjectInfo";
-export { default as DetailButtons } from "@/Partials/project/DetailButtons";
-export { default as Images } from "@/Partials/project/ProjectImages";
-export { default as Contributors } from "@/Partials/project/ProjectMembers";
+export { default as Info          } from "@/Partials/project/ProjectInfo"   ;
+export { default as DetailButtons } from "@/Partials/project/DetailButtons" ;
+export { default as Images        } from "@/Partials/project/ProjectImages" ;
+export { default as Contributors  } from "@/Partials/project/ProjectMembers";
 
-export { default as Loading } from "@/Components/Loading";
+export { default as Loading    } from "@/Components/Loading"       ;
+export { default as Header     } from "@/Components/header/Header" ;
+export { default as Logo       } from "@/Components/logo/Logo"     ;
+export { default as DrawerMenu } from "@/Components/DrawerMenu"    ;
