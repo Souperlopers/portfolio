9219e02e30877777c10ed869e62ad27b431dc3cf
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-05 19:32:24 +0330

    style(TagsComponent): set max height 50px for mobile

--- resources/js/Components/TagsComponent.tsx
+++ resources/js/Components/TagsComponent.tsx
@@ -2,7 +2,7 @@ import { Tag } from "@/types/tag";
 
 const TagsComponent = ({ tags }: { tags: Tag[] }) => {
     return (
-        <div className="flex flex-wrap md:gap-2 gap-1.5 h-8 overflow-hidden">
+        <div className="flex flex-wrap md:gap-2 gap-1.5 lg:h-8 max-h-[50px] overflow-hidden">
             {tags.map((tag) => (
                 <span
                     key={tag.id}
