e305686c696ece53f1d8cc06a6020466a30c7a24
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-05 19:20:34 +0330

    update(ProjectPage): add navigation list and pass to layout

--- resources/js/Pages/Project.tsx
+++ resources/js/Pages/Project.tsx
@@ -1,8 +1,16 @@
 import { ReactNode } from "react";
-import MainLayout from "@/Layouts/MainLayout";
+import Layout from "@/Layouts/Layout";
 import { ProjectPageProps } from "@/types";
 import { Head } from "@inertiajs/react";
 import { Info, Images, Contributors } from "@/index";
+import { NavigationItems           } from "@/types/navigation" ;
+
+const projectPageNavigationItems: NavigationItems[] = [
+    { title: "خانه"                , href : "/"       },
+    { title: "تکنولوژی‌های پروژه"  , id   : "info"    },
+    { title: "توسعه‌دهندگان پروژه" , id   : "members" },
+];
+
 
 export default function Project({ project }: ProjectPageProps) {
     const projectData = project.data;
@@ -10,14 +18,17 @@ export default function Project({ project }: ProjectPageProps) {
     const contributors = projectData.contributors || [];
 
     return (
-        <div className="flex flex-col gap-5 lg:p-5 p-3">
+        <div className="flex flex-col gap-5 lg:p-5 p-3 w-full max-w-[1350px] mt-20">
             <Head title={`SouperLopers ${projectData.title}`} />
             <div className="w-full flex flex-col gap-10">
-                <div id="info" className="flex lg:flex-row flex-col justify-between items-start rounded gap-5">
+                <div
+                    id="info"
+                    className="scroll-mt-20 flex lg:flex-row flex-col justify-between items-start rounded gap-5"
+                >
                     <Info info={projectData} />
                     <Images images={images} />
                 </div>
-                <div className="" id="members">
+                <div className="scroll-mt-20" id="members">
                     <Contributors contributors={contributors} />
                 </div>
             </div>
@@ -25,4 +36,4 @@ export default function Project({ project }: ProjectPageProps) {
     );
 }
 
-Project.layout = (page: ReactNode) => <MainLayout children={page} />;
+Project.layout = (page: ReactNode) => <Layout children={page} navigationList={projectPageNavigationItems} />;
