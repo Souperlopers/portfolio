b3ee05d282b8526393b4a0e4662db46a72836e7c
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-07 20:12:24 +0330

    update(TagsComponent): refactor and add justify as a parameter to set display tags justify content

--- resources/js/Components/TagsComponent.tsx
+++ resources/js/Components/TagsComponent.tsx
@@ -1,16 +1,17 @@
 import { Tag } from "@/types/tag";
 
-const TagsComponent = ({ tags }: { tags: Tag[] }) => {
+const TagsComponent = ({ tags, justify="start" }: { tags: Tag[], justify: string }) => {
+    const justifyContent = justify==="center" ? "justify-center":"justify-start";
+    const containerStyle = "w-full flex flex-wrap md:gap-2 gap-1.5 lg:h-7 max-h-[50px] overflow-hidden " + justifyContent;
+
+    const tagStyle = "inline-flex items-center px-2 md:py-1.5 py-1 md:text-[12px] font-semibold text-xs text-primary bg-[#0078d426] rounded-[4px]";
+    const tagTitleStyle = "truncate max-w-20";
+
     return (
-        <div className="flex flex-wrap md:gap-2 gap-1.5 lg:h-8 max-h-[50px] overflow-hidden">
+        <div className={containerStyle}>
             {tags.map((tag) => (
-                <span
-                    key={tag.id}
-                    dir="ltr"
-                    className="inline-flex items-center px-2 md:py-1 py-0.5 md:text-sm text-xs font-medium
-                        text-sky-400 bg-sky-500/10 border border-sky-500/20 rounded"
-                >
-                    <p className="truncate max-w-20">{tag.title}</p>
+                <span key={tag.id} dir="ltr" className={tagStyle}>
+                    <p className={tagTitleStyle}>{tag.title}</p>
                 </span>
             ))}
         </div>
