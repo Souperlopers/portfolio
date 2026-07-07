ec80b6549d0a0f9a65984868fd983df6e8cb8474
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-07 20:32:11 +0330

    style(MembersSection): add padding

--- resources/js/Partials/home/Members.tsx
+++ resources/js/Partials/home/Members.tsx
@@ -4,8 +4,8 @@ import styles from "./style/MembersStyle.module.css";
 
 const Members = ({ members }: { members: MemberBrief[] }) => {
     return (
-        <section id="members" className="scroll-mt-24 w-full max-w-[1350px] mx-auto px-5">
-            <h2 className="text-xl md:text-2xl lg:text-3xl font-medium text-white mb-5 md:mb-7">
+        <section id="members" className="scroll-mt-24 w-full max-w-[1350px] mx-auto px-10">
+            <h2 className="text-xl md:text-2xl lg:text-3xl font-medium text-white mb-5 md:mb-7 pr-1">
                 توسعه‌دهندگان
             </h2>
             <div className={styles.container}>
