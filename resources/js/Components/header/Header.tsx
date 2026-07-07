6ffa15ba3f4991a89e6638abba705ee301691d9d
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-07 12:46:07 +0330

    update(Header): refactor

--- resources/js/Components/header/Header.tsx
+++ resources/js/Components/header/Header.tsx
@@ -1,18 +1,23 @@
 import { useState        ,useEffect  } from 'react'              ;
 import { NavigationItems             } from "@/types/navigation" ;
-import { usePage                     } from "@inertiajs/react"   ;
 import { motion                      } from "framer-motion"      ;
 import { Logo           , DrawerMenu } from "@/index"            ;
 
+//style
+const constainerStyle = "fixed z-40 w-full max-w-[1350px] flex lg:justify-start justify-between items-center py-3";
+const backgroundColor = "linear-gradient(to top, rgba(51, 51, 51, 0.99), rgba(37, 37, 38, 0.99))";
+
+const itemParentStyle = "lg:flex hidden w-full justify-start gap-8 lg:mr-10 mr-6 font-medium md:text-[20px] text-[15px] text-white";
+
+const navItemStyle       = "relative py-2 transition-all duration-200 focus-visible:outline-none text-white hover:text-white";
+const activeNotItemStyle = "opacity-60 hover:opacity-90"   
+
+const underLineStyle = "absolute bottom-0 left-0 right-0 h-[2px] bg-sky-500";
+
 const Header = ({navigationList}:{navigationList:NavigationItems[]}) => {
-        const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-        const pathname = usePage().url;
-    
+        const [activeSection, setActiveSection] = useState(navigationList[0].id || "");
 
-        const [activeSection, setActiveSection] = useState("projects");
-        const [hoveredItem, setHoveredItem] = useState<string | null>(null);
-
-
+        // scroll
         const scrollToSection = (item: NavigationItems) => {
             if (item.href) {
                 window.location.href = item.href;
@@ -26,9 +31,12 @@ const Header = ({navigationList}:{navigationList:NavigationItems[]}) => {
                 });
             }
         };
+
+        //observer for activate section
         useEffect(() => {
-            const sections = ["projects", "members", "info", "profile", "contact"];
-            const observers: IntersectionObserver[] = [];
+            const sections                          = navigationList.map((item)=>item.id) || [ ] ;
+            const observers: IntersectionObserver[] =                                        [ ] ;
+            setActiveSection(navigationList[0]?.id || "");
 
             const observerOptions = {
                 root: null,
@@ -37,6 +45,7 @@ const Header = ({navigationList}:{navigationList:NavigationItems[]}) => {
             };
 
             sections.forEach((id) => {
+                if (!id) return;
                 const element = document.getElementById(id);
                 if (!element) return;
 
@@ -51,42 +60,37 @@ const Header = ({navigationList}:{navigationList:NavigationItems[]}) => {
             });
 
             return () => observers.forEach((observer) => observer.disconnect());
-        }, []);
+        }, [navigationList]);
 
 
     return (
-        <div className="fixed z-40 w-full max-w-[1350px] flex lg:justify-start justify-between items-center py-3" 
-             style={{background:  "linear-gradient(to top, rgba(51, 51, 51, 0.99), rgba(37, 37, 38, 0.99))"}}>
+        <div className={constainerStyle} style={{background: backgroundColor}}>
             <Logo />
-            <div
-                    className={`lg:flex hidden w-full justify-start gap-8 lg:mr-10 mr-6 font-medium md:text-[20px] text-[15px] text-white`}
-                >
-                    {navigationList.map((item) => (
-                        <button
-                            key={item.title}
-                            onClick={() => scrollToSection(item)}
-                            onMouseEnter={() => setHoveredItem(item.title)}
-                            className={`relative ${activeSection === item.id ? "text-sky-500" : ""} py-2 text-white transition-colors duration-200 focus-visible:outline-none opacity-80 hover:opacity-100`}
-                        >
-                            <span>{item.title}</span>
-
-                            {hoveredItem === item.title && (
-                                <motion.span
-                                    layoutId="nav-underline"
-                                    className={`absolute bottom-0 left-0 right-0 h-[2px] bg-sky-500`}
-                                    transition={{
-                                        type: "spring",
-                                        stiffness: 300,
-                                        damping: 30,
-                                    }}
-                                />
-                            )}
-                        </button>
-                    ))}
-                </div>
-                <div className='lg:hidden flex'>
-                    <DrawerMenu list={navigationList} onItemClick={scrollToSection}/>
-                </div>                
+            <div className={itemParentStyle}>
+                {navigationList.map((item) => (
+                    <button
+                        key={item.title}
+                        onClick={() => scrollToSection(item)}
+                        className={`${navItemStyle} ${activeSection != item.id && activeNotItemStyle}`}
+                    >
+                        <span>{item.title}</span>
+                        {activeSection === item.id && (
+                            <motion.span
+                                layoutId="nav-underline"
+                                className={underLineStyle}
+                                transition={{
+                                    type: "spring",
+                                    stiffness: 200,
+                                    damping: 30,
+                                }}
+                            />
+                        )}
+                    </button>
+                ))}
+            </div>
+            <div className='lg:hidden flex'>
+                <DrawerMenu list={navigationList} onItemClick={scrollToSection}/>
+            </div>                
         </div>
     );
 }
