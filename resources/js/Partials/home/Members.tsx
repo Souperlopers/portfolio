27d2faaca91fa690224235418291cf394590daad
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-08 22:49:56 +0330

    style(MembersSection): increase padding x

--- resources/js/Partials/home/Members.tsx
+++ resources/js/Partials/home/Members.tsx
@@ -4,7 +4,7 @@ import styles from "./style/MembersStyle.module.css";
 
 const Members = ({ members }: { members: MemberBrief[] }) => {
     return (
-        <section id="members" className="scroll-mt-24 w-full max-w-[1350px] mx-auto px-10">
+        <section id="members" className="scroll-mt-24 w-full max-w-[1350px] mx-auto px-20">
             <h2 className="text-xl md:text-2xl lg:text-3xl font-medium text-white mb-5 md:mb-7 pr-1">
                 توسعه‌دهندگان
             </h2>
