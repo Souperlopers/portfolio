044f735d95919cee54b0a9935ec4192f6c9dcd55
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-10 15:23:16 +0330

    update(DetailButton): refactor

--- resources/js/Partials/project/DetailButtons.tsx
+++ resources/js/Partials/project/DetailButtons.tsx
@@ -1,6 +1,9 @@
 import { ProjectLinks } from "@/types/project";
 import { Link } from "@inertiajs/react";
 
+//style
+const buttonStyle = "bg-sky-500 rounded md:w-32 w-24 px-3 py-2 md:font-semibold md:text-base text-sm";
+
 const DetailButtons = ({ links }: { links: ProjectLinks }) => {
     
     const linkLoop = Object.entries(links).filter(
@@ -11,7 +14,7 @@ const DetailButtons = ({ links }: { links: ProjectLinks }) => {
         <>
             {linkLoop.map((link) => (
                 <Link key={link[0]} href={link[1]}>
-                    <button className={`bg-sky-500 rounded w-32 px-3 py-2 font-semibold`}>
+                    <button className={buttonStyle}>
                         <span>{link[0]}</span>
                     </button>
                 </Link>
