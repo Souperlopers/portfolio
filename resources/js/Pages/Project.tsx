5692310dad35a27e55968ac02184acd9f17b3315
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-10 17:22:40 +0330

    update(ProjectPage): refactor

--- resources/js/Pages/Project.tsx
+++ resources/js/Pages/Project.tsx
@@ -5,12 +5,20 @@ import { Head } from "@inertiajs/react";
 import { Info, Images, Contributors } from "@/index";
 import { NavigationItems           } from "@/types/navigation" ;
 
+//style
+const containerStyle = "w-full flex flex-col gap-10";
+
+const infoStyle = "scroll-mt-20 flex lg:flex-row flex-col justify-between items-start rounded gap-5";
+
+const membersStyle = "scroll-mt-20";
+
+const membersTitleStyle = "text-xl md:text-2xl lg:text-3xl font-medium text-white mb-5 md:mb-7 pr-1"
+
 const projectPageNavigationItems: NavigationItems[] = [
     { title: "خانه"                , href : "/"       },
     { title: "تکنولوژی‌های پروژه"  , id   : "info"    },
     { title: "توسعه‌دهندگان پروژه" , id   : "members" },
 ];
-
 
 export default function Project({ project }: ProjectPageProps) {
     const projectData = project.data;
@@ -18,24 +26,21 @@ export default function Project({ project }: ProjectPageProps) {
     const contributors = projectData.contributors || [];
 
     return (
-        <div className="flex flex-col gap-5 lg:py-5 lg:px-20 px-5 w-full max-w-[1350px] mt-24">
+        <>
             <Head title={`SouperLopers ${projectData.title}`} />
-            <div className="w-full flex flex-col gap-10">
-                <div
-                    id="info"
-                    className="scroll-mt-20 flex lg:flex-row flex-col justify-between items-start rounded gap-5"
-                >
+            <div className={containerStyle}>
+                <div id="info" className={infoStyle}>
                     <Info info={projectData} />
                     <Images images={images} />
                 </div>
-                <div className="scroll-mt-20" id="members">
-                    <h2 className="text-xl md:text-2xl lg:text-3xl font-medium text-white mb-5 md:mb-7 pr-1">
+                <div id="members" className={membersStyle}>
+                    <h2 className={membersTitleStyle}>
                      توسعه‌دهندگان
                     </h2>
                     <Contributors contributors={contributors} />
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
