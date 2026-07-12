2c8c4d2e364ae8d5aad714d389b57fede72c10f6
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-12 18:12:53 +0330

    update(Home): add About Section

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,8 +1,8 @@
-import { ReactNode       }                    from "react"                ;
-import   MainLayout                           from "@/Layouts/MainLayout" ;
-import { Banner          ,Projects, Members } from "@/index"              ;
-import { HomePageProps   }                    from "@/types"              ;
-import { NavigationItems }                    from "@/types/navigation"   ;
+import { ReactNode       }                           from "react"                ;
+import   MainLayout                                  from "@/Layouts/MainLayout" ;
+import { Banner          ,Projects, Members, About } from "@/index"              ;
+import { HomePageProps   }                           from "@/types"              ;
+import { NavigationItems }                           from "@/types/navigation"   ;
 
 const homePageNavigationItems: NavigationItems[] = [
     { title: "نمونه کار", id: "projects" },
@@ -16,6 +16,7 @@ export default function Home({ projects, members }: HomePageProps) {
             <Banner />
             <Projects projects={projects.data} />
             <Members members={members.data} />
+            <About />
         </>
     );
 }
