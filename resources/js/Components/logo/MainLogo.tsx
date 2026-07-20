0ed368deea085c56ad1650bd4dc89d5838187251
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-20 15:53:05 +0330

    logo animation timing

--- resources/js/Components/logo/MainLogo.tsx
+++ resources/js/Components/logo/MainLogo.tsx
@@ -26,28 +26,27 @@ export default function MainLogo() {
 
     return (
         <Link
-            className={` ${scrolled ? "fixed top-2 -right-5" : "absolute -translate-x-20 translate-y-20"} hover: z-50 transform duration-1000`}
+            className={` ${scrolled ? "fixed top-2 -right-5" : "absolute -translate-x-20 translate-y-20"} z-50 duration-700`}
             dir="ltr"
             href="/"
         >
             <div
                 className={`${scrolled ? "h-[70px]" : "h-[150px]"} aspect-[330/150] relative cursor-pointer ease-linear`}
-                style={{ transition: "width 0.2s " }}
             >
                 <img
                     src={SouperSrc}
                     alt="Souper"
-                    className={`w-[45%] h-[60%] ${scrolled ? "top-[3%] left-[33.33%]" : "top-1/2 -translate-y-1/2 left-0"} absolute transition-position will-change-transform duration-1000 ease-in-out`}
+                    className={`w-[45%] h-[60%] ${scrolled ? "top-[3%] left-[33.33%]" : "top-1/2 -translate-y-1/2 left-0"} absolute transition-position will-change-transform duration-700 ease-in-out`}
                 />
                 <img
                     src={LopersSrc}
                     alt="Lopers"
-                    className={`w-[45%] h-[60%] ${scrolled ? "left-[21.21%] bottom-[3%]" : "bottom-1/2 translate-y-1/2 left-[45%]"} absolute transition-position will-change-transform duration-1000 ease-in-out`}
+                    className={`w-[45%] h-[60%] ${scrolled ? "left-[21.21%] bottom-[3%]" : "bottom-1/2 translate-y-1/2 left-[45%]"} absolute transition-position will-change-transform duration-700 ease-in-out`}
                 />
                 <img
                     src={PtxSrc}
                     alt="Gul"
-                    className={`w-[8%] h-[40%] ${scrolled ? "right-[22%] bottom-[15%] rotate-[80deg]" : "opacity-0 bottom-1/2 translate-y-1/2 right-0"} absolute transition-position will-change-transform duration-300 ease-in-out`}
+                    className={`w-[8%] h-[40%] ${scrolled ? "right-[22%] bottom-[15%] rotate-[80deg]" : "opacity-0 bottom-1/2 translate-y-1/2 right-0"} absolute transition-position will-change-transform duration-700 ease-in-out`}
                 />
             </div>
         </Link>
