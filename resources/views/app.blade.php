85b231309a8ad323ea15734a447e4eca977b7b11
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-20 12:21:17 +0330

    fix layout shift on scrollbar appearing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
         @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
-    <body>
+    <body class="overflow-y-scroll">
         @inertia
     </body>
 </html>
