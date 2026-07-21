b9dcf7868bb98cea50bb6ae59ae8d3eee06768cd
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-21 13:44:32 +0330

    update(TagComponents): add icon and set new style

--- resources/js/Components/TagsComponent.tsx
+++ resources/js/Components/TagsComponent.tsx
@@ -1,21 +1,27 @@
+import { useState } from "react";
 import { Tag } from "@/types/tag";
 
-const TagsComponent = ({ tags, justify="start" }: { tags: Tag[], justify: string }) => {
-    const justifyContent = justify==="center" ? "justify-center":"justify-start";
-    const containerStyle = "w-full flex flex-wrap md:gap-2 gap-1.5 lg:h-7 max-h-[50px] overflow-hidden " + justifyContent;
-
-    const tagStyle = "inline-flex items-center px-2 md:py-1.5 py-1 md:text-[12px] font-semibold text-xs text-primary bg-[#0078d426] rounded-[4px]";
-    const tagTitleStyle = "truncate max-w-20";
-
+export default function ({ tags }: { tags: Tag[] }) {
+    const [hasIcon, setHasIcon] = useState(true);
     return (
-        <div className={containerStyle}>
+        <div className={`flex gap-2 flex-wrap`}>
             {tags.map((tag) => (
-                <span key={tag.id} dir="ltr" className={tagStyle}>
-                    <p className={tagTitleStyle}>{tag.title}</p>
-                </span>
+                <div
+                    key={tag.id}
+                    dir="ltr"
+                    className={`flex items-center gap-2 bg-white/5 rounded-xl px-4 py-2 text-sm`}
+                >
+                    {hasIcon && (
+                        <img
+                            className="w-5 h-5"
+                            src={`/assets/icons/${tag.title}.svg`}
+                            alt={tag.title}
+                            onError={() => setHasIcon(false)}
+                        />
+                    )}
+                    <span className={`truncate max-w-20`}>{tag.title}</span>
+                </div>
             ))}
         </div>
     );
-};
-
-export default TagsComponent;
+}
