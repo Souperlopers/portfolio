7df38280d7d58dadd4bbfb5c560ab9e001c58c1d
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-22 00:17:16 +0330

    fixed partial background color when content is few

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
         @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
-    <body dir="rtl" class="overflow-y-scroll font-vazir bg-base-300">
+    <body dir="rtl" class="overflow-y-scroll min-h-screen font-vazir bg-base-300">
         @inertia
     </body>
 </html>
