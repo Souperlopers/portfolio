2caa99ed6f99a5f19b0aec11987b4bbc1362e298
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-21 14:14:05 +0330

    update(MainLayout): add loading

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -1,24 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ReactNode } from "react";
 import { Header, Loading } from "@/index";
 import { HeaderData } from "@/types/navigation";
+import { router } from "@inertiajs/react";
 
-export default function MainLayout({ children, HeaderData}: { children: ReactNode, HeaderData: HeaderData}) {
+export default function MainLayout({
+    children,
+    HeaderData,
+}: {
+    children: ReactNode;
+    HeaderData: HeaderData;
+}) {
     const [isloading, setIsloading] = useState(false);
+    const navigationList = HeaderData.navList;
 
-    /* does this even work?
-        const [isloading, setIsloading] = useState(false);
+    useEffect(() => {
+        const removeStart = router.on("start", () => setIsloading(true));
+        const removeFinish = router.on("finish", () => setIsloading(false));
 
-        useEffect(() => {
-            const removeStart = router.on("start", () => setIsloading(true));
-            const removeFinish = router.on("finish", () => setIsloading(false));
-
-            return () => {
-                removeStart();
-                removeFinish();
-            };
-        }, [navigationList]);
-    */
+        return () => {
+            removeStart();
+            removeFinish();
+        };
+    }, [navigationList]);
 
     return (
         <>
