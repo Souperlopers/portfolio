aa0c1473ebee1eb0cc97d6e22c90811c57a1cc13
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-25 14:00:35 +0330

    update(Logo, Hero): add skeleton

--- resources/js/Components/logo/Logo.tsx
+++ resources/js/Components/logo/Logo.tsx
@@ -7,6 +7,11 @@ export default function Logo({ isCompact = true }: { isCompact: boolean }) {
     const [target, setTarget] = useState<HTMLElement | null>(null);
     const [timePassed, setTimePassed] = useState(false);
 
+    const [loadedCount, setLoadedCount] = useState(0);
+    const allLoaded = loadedCount >= 3;
+
+    const onLoad = () => setLoadedCount((prev) => prev + 1);
+
     const ANIMATION_TIME = 200;
 
     const relatedPositions = {
@@ -33,34 +38,58 @@ export default function Logo({ isCompact = true }: { isCompact: boolean }) {
         return () => clearTimeout(tId);
     }, [isCompact]);
 
-    type StateType = "compact_start" | "compact_end" | "wide_start" | "wide_end"
+    type StateType =
+        | "compact_start"
+        | "compact_end"
+        | "wide_start"
+        | "wide_end";
     const animationState: StateType = isCompact
-        ? (timePassed ? "compact_start" : "compact_end")
-        : (timePassed ? "wide_start" : "wide_end")
-    const isOnTop = (animationState === "compact_end" || animationState === "wide_start")
+        ? timePassed
+            ? "compact_start"
+            : "compact_end"
+        : timePassed
+          ? "wide_start"
+          : "wide_end";
+    const isOnTop =
+        animationState === "compact_end" || animationState === "wide_start";
 
     return !target
         ? null
         : createPortal(
-            <AnimatePresence mode="wait">
-                <motion.div
-                    key={isCompact ? "compact" : "wide"}
-                    initial={relatedPositions}
-                    exit={relatedPositions}
-                    animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
-                    transition={{ duration: ANIMATION_TIME / 1000, ease: "linear" }}
-                    className={`h-full w-full z-50 relative`}
-                >
-                    <Link href="/">
-                        <img src={`/assets/images/logo-images/souper.svg`} alt="Souper"
-                            className={`absolute ${isOnTop ? "w-[45%] top-0 left-0" : "w-[80%] top-0 left-[20%"} transition-all will-change-transform duration-700 ease-out `} />
-                        <img src={`/assets/images/logo-images/lopers.svg`} alt="Lopers"
-                            className={`absolute ${isOnTop ? "w-[43.76%] bottom-0 left-[45%]" : "w-[80%] bottom-0 left-0"} transition-all will-change-transform duration-700 ease-out `} />
-                        <img src={`/assets/images/logo-images/ptx.svg`} alt="Gul"
-                            className={`absolute ${isOnTop ? "w-[11.24%] bottom-1/2 translate-y-1/2 right-0" : "w-[8%] bottom-[5%] right-[5%] rotate-[80deg]"} transition-all will-change-transform duration-700 ease-out `} />
-                    </Link>
-                </motion.div>
-            </AnimatePresence>,
-            target,
-        );
+              <AnimatePresence mode="wait">
+                  <motion.div
+                      key={isCompact ? "compact" : "wide"}
+                      initial={relatedPositions}
+                      exit={relatedPositions}
+                      animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+                      transition={{
+                          duration: ANIMATION_TIME / 1000,
+                          ease: "linear",
+                      }}
+                      className={`h-full w-full z-50 relative`}
+                  >
+                      <Link href="/">
+                          <img
+                              src={`/assets/images/logo-images/souper.svg`}
+                              alt="Souper"
+                              onLoad={onLoad}
+                              className={`absolute ${isOnTop ? "w-[45%] top-0 left-0" : "w-[80%] top-0 left-[20%"} transition-all will-change-transform duration-700 ease-out ${allLoaded ? "block" : "hidden"}`}
+                          />
+                          <img
+                              src={`/assets/images/logo-images/lopers.svg`}
+                              alt="Lopers"
+                              onLoad={onLoad}
+                              className={`absolute ${isOnTop ? "w-[43.76%] bottom-0 left-[45%]" : "w-[80%] bottom-0 left-0"} transition-all will-change-transform duration-700 ease-out ${allLoaded ? "block" : "hidden"}`}
+                          />
+                          <img
+                              src={`/assets/images/logo-images/ptx.svg`}
+                              alt="Gul"
+                              onLoad={onLoad}
+                              className={`absolute ${isOnTop ? "w-[11.24%] bottom-1/2 translate-y-1/2 right-0" : "w-[8%] bottom-[5%] right-[5%] rotate-[80deg]"} transition-all will-change-transform duration-700 ease-out ${allLoaded ? "block" : "hidden"}`}
+                          />
+                      </Link>
+                  </motion.div>
+              </AnimatePresence>,
+              target,
+          );
 }
--- resources/js/Partials/home/Hero.tsx
+++ resources/js/Partials/home/Hero.tsx
@@ -6,59 +6,64 @@ export default function Hero() {
     const onLoad = () => setImagesLoadedCount((prev) => prev + 1);
 
     return (
-        <div className="h-[55vh] w-full relative overflow-hidden">
-            {/* white section */}
-            <img
-                className={`absolute aspect-[3042/510] max-w-[350vw] translate-x-[50vw]`}
-                src="/assets/images/banner-images/Subtract2.svg"
-                alt="banner white section"
-                onLoad={onLoad}
-            />
+        <>
+            {!allLoaded && <div className="skeleton h-[55vh] w-full"></div>}
+            <div className="h-[55vh] w-full relative overflow-hidden">
+                {/* white section */}
+                <img
+                    className={`absolute aspect-[3042/510] max-w-[350vw] translate-x-[50vw] ${allLoaded ? "block" : "hidden"}`}
+                    src="/assets/images/banner-images/Subtract2.svg"
+                    alt="banner white section"
+                    onLoad={onLoad}
+                />
 
-            {/* logo */}
-            <div id="hero-logo" className={`z-50 absolute w-3/4 sm: aspect-[313/90] top-[15%] left-1/2 -translate-x-1/2`}></div>
+                {/* logo */}
+                <div
+                    id="hero-logo"
+                    className={`z-50 absolute w-3/4 sm: aspect-[313/90] top-[15%] left-1/2 -translate-x-1/2`}
+                ></div>
 
-            {/* describtion */}
-            <p className="absolute z-50 text-2xl xs:text-3xl text-balance w-3/4 bottom-0 pb-4 left-1/2 -translate-x-1/2">
-                تولیدکننده انواع نرم‌افزارهای اداری و تجاری
-            </p>
+                {/* describtion */}
+                <p className="absolute z-50 text-2xl xs:text-3xl text-balance w-3/4 bottom-0 pb-4 left-1/2 -translate-x-1/2">
+                    تولیدکننده انواع نرم‌افزارهای اداری و تجاری
+                </p>
 
-            <div className="hidden sm:visible absolute h-[40vh] aspect-[1086/684] left-1/2 -translate-x-1/2 top-1/2">
-                <div className="relative w-full h-full">
-                    <img // blue section
-                        src="/assets/images/banner-images/Subtract.svg"
-                        alt="blue section of banner"
-                        onLoad={onLoad}
-                        className={`absolute z-10 w-full h-full`}
-                    />
+                <div className="hidden sm:visible absolute h-[40vh] aspect-[1086/684] left-1/2 -translate-x-1/2 top-1/2">
+                    <div className="relative w-full h-full">
+                        <img // blue section
+                            src="/assets/images/banner-images/Subtract.svg"
+                            alt="blue section of banner"
+                            onLoad={onLoad}
+                            className={`absolute z-10 w-full h-full ${allLoaded ? "block" : "hidden"}`}
+                        />
 
-                    <img // plant
-                        src="/assets/images/banner-images/plant.svg"
-                        alt="banner plant"
-                        onLoad={onLoad}
-                        className={`absolute z-40 w-[6%] left-[17%] top-[40%] opacity-50`}
-                    />
-                    <img // girl
-                        src="/assets/images/banner-images/Girl.svg"
-                        alt="banner girl"
-                        onLoad={onLoad}
-                        className={`absolute z-40 w-[14%] bottom-[8%] left-[35%]`}
-                    />
-                    <img // boy
-                        src="/assets/images/banner-images/MAAAN.svg"
-                        alt="banner man"
-                        onLoad={onLoad}
-                        className={`absolute z-40 w-[28%] right-[20%] bottom-[15%]`}
-                    />
-                    <img // monitor
-                        src="/assets/images/banner-images/monitor.svg"
-                        alt="banner monitor"
-                        onLoad={onLoad}
-                        className={`-translate-x-1/2 -translate-y-1/2 absolute z-40 w-[20%] left-[55%] top-[45%]`}
-                    />
+                        <img // plant
+                            src="/assets/images/banner-images/plant.svg"
+                            alt="banner plant"
+                            onLoad={onLoad}
+                            className={`absolute z-40 w-[6%] left-[17%] top-[40%] opacity-50 ${allLoaded ? "block" : "hidden"}`}
+                        />
+                        <img // girl
+                            src="/assets/images/banner-images/Girl.svg"
+                            alt="banner girl"
+                            onLoad={onLoad}
+                            className={`absolute z-40 w-[14%] bottom-[8%] left-[35%] ${allLoaded ? "block" : "hidden"}`}
+                        />
+                        <img // boy
+                            src="/assets/images/banner-images/MAAAN.svg"
+                            alt="banner man"
+                            onLoad={onLoad}
+                            className={`absolute z-40 w-[28%] right-[20%] bottom-[15%] ${allLoaded ? "block" : "hidden"}`}
+                        />
+                        <img // monitor
+                            src="/assets/images/banner-images/monitor.svg"
+                            alt="banner monitor"
+                            onLoad={onLoad}
+                            className={`-translate-x-1/2 -translate-y-1/2 absolute z-40 w-[20%] left-[55%] top-[45%] ${allLoaded ? "block" : "hidden"}`}
+                        />
+                    </div>
                 </div>
             </div>
-
-        </div>
+        </>
     );
 }
