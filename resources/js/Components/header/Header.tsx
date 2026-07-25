628981d709b399eabd7ac08019d50464d57351d6
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-25 21:01:03 +0330

    removed header shadow whne its below hero add descriptive comments format hamburge line to multiline

--- resources/js/Components/header/Header.tsx
+++ resources/js/Components/header/Header.tsx
@@ -56,8 +56,8 @@ export default function Header({
         return () => observers.forEach((observer) => observer.disconnect());
     }, [navigationList]);
 
+    // calculate header breakpoint when hero exists
     const [isScrolled, setScrolled] = useState(false);
-
     useEffect(() => {
         const handleScroll = () => {
             const scrollY = window.scrollY;
@@ -74,6 +74,7 @@ export default function Header({
         return () => window.removeEventListener("scroll", handleScroll);
     });
 
+    // determine header position
     const isBelowHero = hasHero && !isScrolled // other pages || scrolled hero page
 
     return (
@@ -81,7 +82,7 @@ export default function Header({
             <Logo isCompact={hasHero ? isScrolled : true} />
             {hasHero && <Hero />}
             <header
-                className={`sticky top-0 z-50 flex items-center shadow-[0_10px_30px_rgba(255,255,255,.05)] ${isBelowHero ? "h-[45vh] flex-col" : "px-5 justify-between sm:justify-start h-[80px] py-2 bg-gradient-to-b from-[#010103] via-[#050912]/90 to-[#121927]/80 backdrop-blur-lg"} transition-all will-change-transform duration-300 ease-in-out`}
+                className={`sticky top-0 z-50 flex items-center ${isBelowHero ? "h-[45vh] flex-col" : "px-5 shadow-[0_10px_30px_rgba(255,255,255,.05)] justify-between sm:justify-start h-[80px] py-2 bg-gradient-to-b from-[#010103] via-[#050912]/90 to-[#121927]/80 backdrop-blur-lg"} transition-all will-change-transform duration-300 ease-in-out`}
             >
                 <div id="header-logo" className={`${isBelowHero && "hidden"} aspect-[200/150] h-[50px] transition-all will-change-transform duration-300 ease-in-out`}></div>
                 <nav
@@ -97,7 +98,11 @@ export default function Header({
                         </button>
                     ))}
                 </nav>
-                {!isBelowHero && <Hamburger navList={navigationList} classNames={`${isBelowHero ? "hidden" : "visible sm:hidden"}`} clickHandler={scrollToSection} />}
+                {!isBelowHero && <Hamburger
+                    navList={navigationList}
+                    classNames={`${isBelowHero ? "hidden" : "visible sm:hidden"}`}
+                    clickHandler={scrollToSection}
+                />}
             </header>
         </>
     );
