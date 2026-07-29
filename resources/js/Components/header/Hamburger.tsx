52384dc5dd7f57120783d1418ac7dd53ac530be4
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-29 17:31:04 +0330

    update(Hamburger): set min width

--- resources/js/Components/header/Hamburger.tsx
+++ resources/js/Components/header/Hamburger.tsx
@@ -3,92 +3,92 @@ import clsx from "clsx/lite"
 import { useEffect, useRef, useState } from "react"
 
 export default function Hamburger({
-    navList,
-    classNames,
-    clickHandler,
+	navList,
+	classNames,
+	clickHandler,
 }: {
-    navList: NavigationItem[]
-    classNames: string
-    clickHandler: (item: NavigationItem) => void
+	navList: NavigationItem[]
+	classNames: string
+	clickHandler: (item: NavigationItem) => void
 }) {
-    const [isOpen, setIsOpen] = useState(false)
-    const menuRef = useRef<HTMLDivElement>(null)
-    const buttonRef = useRef<HTMLButtonElement>(null)
+	const [isOpen, setIsOpen] = useState(false)
+	const menuRef = useRef<HTMLDivElement>(null)
+	const buttonRef = useRef<HTMLButtonElement>(null)
 
-    const toggleMenu = () => setIsOpen((prev) => !prev)
+	const toggleMenu = () => setIsOpen((prev) => !prev)
 
-    const handleClickOutside = (e: MouseEvent) => {
-        if (
-            menuRef.current &&
-            !menuRef.current.contains(e.target as Node) &&
-            buttonRef.current &&
-            !buttonRef.current.contains(e.target as Node)
-        ) {
-            setIsOpen(false)
-        }
-    }
+	const handleClickOutside = (e: MouseEvent) => {
+		if (
+			menuRef.current &&
+			!menuRef.current.contains(e.target as Node) &&
+			buttonRef.current &&
+			!buttonRef.current.contains(e.target as Node)
+		) {
+			setIsOpen(false)
+		}
+	}
 
-    useEffect(() => {
-        document.addEventListener("mousedown", handleClickOutside)
-        return () =>
-            document.removeEventListener("mousedown", handleClickOutside)
-    }, [])
+	useEffect(() => {
+		document.addEventListener("mousedown", handleClickOutside)
+		return () =>
+			document.removeEventListener("mousedown", handleClickOutside)
+	}, [])
 
-    return (
-        <div ref={menuRef} className={`dropdown text-center ${classNames}`}>
-            <button
-                ref={buttonRef}
-                onClick={toggleMenu}
-                tabIndex={0}
-                role="button"
-                className="btn btn-circle btn-ghost"
-            >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="pointer-events-none h-10 w-10"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h16M4 18h7"
-                    />
-                </svg>
-            </button>
+	return (
+		<div ref={menuRef} className={`dropdown text-left ${classNames}`}>
+			<button
+				ref={buttonRef}
+				onClick={toggleMenu}
+				tabIndex={0}
+				role="button"
+				className="btn btn-circle btn-ghost"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					className="pointer-events-none h-10 w-10"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="2"
+						d="M4 6h16M4 12h16M4 18h7"
+					/>
+				</svg>
+			</button>
 
-            <ul
-                id="hamburger-menu"
-                role="menu"
-                tabIndex={-1}
-                className={clsx(
-                    isOpen ? "block" : "hidden", // toggling logic
-                    "text-2xl", // text
-                    "menu-xl menu dropdown-content", // daisyui menu
-                    "w-fit", // demension
-                    "mt-3 p-2", // padding and margin
-                    "left-0", // position
-                    "rounded-box bg-base-100 shadow", // style
-                )}
-            >
-                {navList.map((navEl) => (
-                    <li key={navEl.id} role="none">
-                        <button
-                            role="menuitem"
-                            onClick={(e) => {
-                                e.preventDefault()
-                                clickHandler(navEl)
-                                setIsOpen(false)
-                            }}
-                            className="w-full"
-                        >
-                            {navEl.content}
-                        </button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+			<ul
+				id="hamburger-menu"
+				role="menu"
+				tabIndex={-1}
+				className={clsx(
+					isOpen ? "block" : "hidden", // toggling logic
+					"text-lg", // text
+					"menu-xl menu dropdown-content", // daisyui menu
+					"min-w-56", // demension
+					"mt-3 p-1.5", // padding and margin
+					"left-0", // position
+					"rounded-box bg-base-100 shadow", // style
+				)}
+			>
+				{navList.map((navEl) => (
+					<li key={navEl.id} role="none">
+						<button
+							role="menuitem"
+							onClick={(e) => {
+								e.preventDefault()
+								clickHandler(navEl)
+								setIsOpen(false)
+							}}
+							className={clsx("w-full")}
+						>
+							{navEl.content}
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
