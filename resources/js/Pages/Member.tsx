f42354bbe748341d9f74584acb725c1b398f411e
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-29 17:13:20 +0330

    update(Member): use contact data

--- resources/js/Pages/Member.tsx
+++ resources/js/Pages/Member.tsx
@@ -9,61 +9,62 @@ import { CgWebsite } from "react-icons/cg"
 import { IoCall } from "react-icons/io5"
 
 export default function Member({ member }: MemberPageProps) {
-    const info = member.data || {}
-    const projects = member.data.contributions || []
+	const info = member.data || {}
+	const projects = member.data.contributions || []
+	const contact = member.data.contact || {}
 
-    return (
-        <div className="flex flex-col gap-10 md:gap-16">
-            <Head title={info.name} />
-            <div id="profile" className={`scroll-mt-20`}>
-                <Profile info={info} />
-            </div>
-            <div id="projects" className={`scroll-mt-20`}>
-                <MemberProjects projects={projects} />
-            </div>
-            <div id="contact" className={`scroll-mt-20`}>
-                <Contact info={info} />
-            </div>
-        </div>
-    )
+	return (
+		<div className="flex flex-col gap-10 md:gap-16">
+			<Head title={info.name} />
+			<div id="profile" className={`scroll-mt-20`}>
+				<Profile info={info} />
+			</div>
+			<div id="projects" className={`scroll-mt-20`}>
+				<MemberProjects projects={projects} />
+			</div>
+			<div id="contact" className={`scroll-mt-20`}>
+				<Contact contact={contact} />
+			</div>
+		</div>
+	)
 }
 
 Member.layout = (page: ReactNode) => (
-    <MainLayout
-        children={page}
-        navigationList={[
-            { content: homeContent, href: "/" },
-            { content: profileContent, id: "profile" },
-            { content: projectsContent, id: "projects" },
-            { content: contactContent, id: "contact" },
-        ]}
-    />
+	<MainLayout
+		children={page}
+		navigationList={[
+			{ content: homeContent, href: "/" },
+			{ content: profileContent, id: "profile" },
+			{ content: projectsContent, id: "projects" },
+			{ content: contactContent, id: "contact" },
+		]}
+	/>
 )
 
 const homeContent = (
-    <>
-        <AiFillHome />
-        <span>خانه</span>
-    </>
+	<>
+		<AiFillHome />
+		<span>خانه</span>
+	</>
 )
 
 const profileContent = (
-    <>
-        <IoCodeSlashOutline />
-        <span>مهارت‌ها</span>
-    </>
+	<>
+		<IoCodeSlashOutline />
+		<span>مهارت‌ها</span>
+	</>
 )
 
 const projectsContent = (
-    <>
-        <CgWebsite />
-        <span>پروژه‌های مربوطه</span>
-    </>
+	<>
+		<CgWebsite />
+		<span>پروژه‌های مربوطه</span>
+	</>
 )
 
 const contactContent = (
-    <>
-        <IoCall />
-        <span>تماس با توسعه‌دهنده</span>
-    </>
+	<>
+		<IoCall />
+		<span>تماس با توسعه‌دهنده</span>
+	</>
 )
