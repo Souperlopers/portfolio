3db1dea8028a70d4804d6ac05e2209f0f35f920f
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-08-02 19:55:39 +0330

    fix loading background

--- resources/js/Components/Loading.tsx
+++ resources/js/Components/Loading.tsx
@@ -1,7 +1,18 @@
+import clsx from "clsx/lite";
+
 export default function Loading() {
     return (
-        <div className="fixed inset-0 z-[100] flex h-dvh w-full items-center justify-center bg-stone-950">
-            <span className="md:loading-xl loading loading-dots loading-md"></span>
+        <div className={clsx(
+			"fixed inset-0 z-[100]", // base
+			"flex items-center justify-center", // flex
+			"h-dvh w-full", // dimension
+			"bg-base-300 dark:bg-stone-950", // background
+		)}>
+            <span className={
+				clsx(
+					"md:loading-xl loading loading-dots loading-md", // daisyui loading
+				)
+			} />
         </div>
     )
 }
