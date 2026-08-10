9329ae9ba49475c956a37e45cf07d8e35073e639
Zahra Rostami <zahrarostami137878@gmail.com>
2026-08-10 17:43:21 +0330

    replace css files with tailwind classes

--- resources/js/Partials/home/Members.tsx
+++ resources/js/Partials/home/Members.tsx
@@ -1,6 +1,5 @@
 import MemberCard from "@/Components/MemberCard"
 import { MemberBrief } from "@/types/member"
-import styles from "./style/MembersStyle.module.css"
 import clsx from "clsx"
 
 export default function Members({ members }: { members: MemberBrief[] }) {
@@ -25,7 +24,14 @@ export default function Members({ members }: { members: MemberBrief[] }) {
 			>
 				اعضا
 			</h2>
-			<div className={styles.container}>
+			<div
+				className={clsx(
+					"grid grid-cols-[repeat(auto-fit,300px)] gap-5", // grid,
+					"justify-items-center", // justify items
+					"justify-around max-[650px]:justify-center", // justify content
+					"min-[650px]:px-20", // padding
+				)}
+			>
 				{members.map((member) => (
 					<MemberCard key={member.id} memberData={member} />
 				))}
--- resources/js/Partials/project/ProjectMembers.tsx
+++ resources/js/Partials/project/ProjectMembers.tsx
@@ -1,6 +1,6 @@
 import MemberCard from "@/Components/MemberCard"
 import { MemberBrief } from "@/types/member"
-import styles from "../home/style/MembersStyle.module.css"
+import clsx from "clsx"
 
 export default function ProjectMembers({
 	contributors,
@@ -8,7 +8,14 @@ export default function ProjectMembers({
 	contributors: MemberBrief[]
 }) {
 	return (
-		<div className={styles.container}>
+		<div
+			className={clsx(
+				"grid grid-cols-[repeat(auto-fit,300px)] gap-5", // grid,
+				"justify-items-center", // justify items
+				"justify-around max-[650px]:justify-center", // justify content
+				"min-[650px]:px-20", // padding
+			)}
+		>
 			{contributors.map((contributor) => (
 				<MemberCard key={contributor.id} memberData={contributor} />
 			))}
