c47911b5cd9dc733ddd991d1c04c29cc20fdebc9
Zahra Rostami <zahrarostami137878@gmail.com>
2026-08-12 19:16:21 +0330

    set vazir font fallback

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="/favicon.ico" />
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        @routes
-        @viteReactRefresh
-        @vite("resources/js/app.tsx")
-        <x-inertia::head />
-    </head>
-    <body dir="rtl" class="overflow-y-scroll min-h-screen font-vazir bg-base-300">
-        <x-inertia::app />
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="/favicon.ico" />
+    <link rel="preload" href="/assets/fonts/Vazirmatn-Regular.woff2" as="font" type="font/woff2" crossorigin />
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    @routes
+    @viteReactRefresh
+    @vite('resources/js/app.tsx')
+    <x-inertia::head />
+</head>
+
+<body dir="rtl" class="overflow-y-scroll min-h-screen font-vazir bg-base-300">
+    <x-inertia::app />
+</body>
+
 </html>
