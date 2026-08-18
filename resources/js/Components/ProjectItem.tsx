443e534c6d6358221cff66a6e3c6d0f79221ff73
Zahra Rostami <zahrarostami137878@gmail.com>
2026-08-18 18:28:38 +0330

    style(ProjectItem): delete opacity-80

--- resources/js/Components/ProjectItem.tsx
+++ resources/js/Components/ProjectItem.tsx
@@ -71,7 +71,9 @@ export default function ProjectItem({ projectData }: ProjectItemProps) {
 					className={clsx(
 						"flex flex-col items-stretch", // flex
 						"h-full w-full", // dimension
-						projectData.thumbnail && !imgLoaded && "aspect-video skeleton", // show 16/9 skeleton
+						projectData.thumbnail &&
+							!imgLoaded &&
+							"skeleton aspect-video", // show 16/9 skeleton
 						projectData.thumbnail && imgLoaded && "block",
 					)}
 				>
@@ -82,7 +84,6 @@ export default function ProjectItem({ projectData }: ProjectItemProps) {
 							onLoad={() => setImgLoaded(true)}
 							className={clsx(
 								"max-h-60 sm:max-h-80", // dimension
-								"opacity-80", // basic
 								"object-contain", // object
 							)}
 						/>
