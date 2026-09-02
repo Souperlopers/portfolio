38fccf691fbb3a398a5d66ebed41ebadc52dc425
Zahra Rostami <zahrarostami137878@gmail.com>
2026-09-02 19:19:37 +0330

    update(ProjectInfo): remove technologies if there are no tags

--- resources/js/Partials/project/ProjectInfo.tsx
+++ resources/js/Partials/project/ProjectInfo.tsx
@@ -5,6 +5,7 @@ import clsx from "clsx/lite"
 
 export default function ProjectInfo({ info }: { info: Project }) {
 	const technologies = info.technologies || []
+
 	return (
 		<div className="text-primary-content flex min-h-100 w-full flex-col justify-between gap-7 pt-5 lg:max-w-[40%]">
 			<div className="text-base-content max-w-80 truncate text-3xl font-semibold md:max-w-96">
@@ -15,11 +16,16 @@ export default function ProjectInfo({ info }: { info: Project }) {
 					"text-secondary-content", // color
 					"text-base md:text-lg", // size
 					"text-justify", // justify
-					)}
+				)}
 			>
 				{info.description}
 			</div>
-			<div className="flex flex-wrap gap-5">
+			<div
+				className={clsx(
+					"flex flex-wrap gap-5", // flex
+					technologies.length === 0 && "hidden", // hide if there are no tags
+				)}
+			>
 				<h2 className="text-base-content text-lg md:text-xl">
 					تکنولوژی‌ها
 				</h2>
